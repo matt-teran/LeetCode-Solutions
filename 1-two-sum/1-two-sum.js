c805cbaa12d58f5b508d39fd03441803a4b394e2
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-06-02 14:15:43 -0500

    Time: 67 ms (94.25%), Space: 42.4 MB (76.80%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -1,9 +1,11 @@
 var twoSum = function(nums, target) {
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] + nums[j] === target) {
-        return [i, j];
-      }
+    let map = {};
+    
+    for (let i = 0; i < nums.length; i++) {
+        if (target - nums[i] in map) {
+            return [map[target - nums[i]], i];
+        } else {
+            map[nums[i]] = i;
+        }
     }
-  }
 };
