1b0a429915a8e78f42f63adc567edddb7d281e8e
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-06-02 13:53:21 -0500

    Time: 93 ms (74.22%), Space: 43.3 MB (73.99%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+
+// inputs: string, string
+// output: boolean 
+// constraints: string lengths > 0 | all lowercase letters
+// edge cases: na
+var isAnagram = function(s, t) {
+    //compare lengths, if not same, return false
+    if (s.length !== t.length) return false;
+    // create a hashmap of the freq of characters in both strings
+    let sMap = {};
+    let tMap = {};
+    for (let i = 0; i < s.length; i++) {
+        s[i] in sMap ? sMap[s[i]]++ : sMap[s[i]] = 1;
+        t[i] in tMap ? tMap[t[i]]++ : tMap[t[i]] = 1;
+    }
+    // compare hashmaps, if any properties are different, return false
+    for (const [k, v] of Object.entries(sMap)) {
+        if (!(k in tMap)) {
+            return false;
+        } else if (v !== tMap[k]) {
+            return false;
+        }
+    }
+    return true;
+};
