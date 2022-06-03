00035e3355503f91e7b0ea7ab439088c68484fa6
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-06-03 11:01:04 -0500

    Time: 2247 ms (10.51%), Space: 62.1 MB (14.35%) - LeetHub

---
+++ 15-3sum/15-3sum.js
@@ -0,0 +1,36 @@
+// inputs: integer array
+// output: integer triplets that add up to 0
+// constraints: na
+// edge cases: nums length 0
+
+var threeSum = function (nums) {
+    if (nums.length < 3) return [];
+    let result = new Set();
+    // sort array
+    nums.sort((a, b) => a - b);
+    // loop through array
+    for (let i = 0; i < nums.length; i++) {
+        let target = 0 - nums[i];
+        // for each itn. set the target to be 0 - itn
+        let j = i + 1;
+        let k = nums.length - 1;
+        while (j < k) {
+            if (nums[j] + nums[k] > target) {
+                k--;
+            } else if (nums[j] + nums[k] < target) {
+                j++;
+            } else {
+                result.add(JSON.stringify([nums[i], nums[j], nums[k]]));
+                j++;
+            }
+        }
+        // use two pointers in sub array to find target
+        // when target is reached, push nums to result and increment i    
+    }
+    //remove duplicates
+    
+    return [...result].map(arr => JSON.parse(arr));
+};
+// [-1,0,1,2,-1,-4]
+// [-4, -1, -1, 0, 1, 2]
+// 0
