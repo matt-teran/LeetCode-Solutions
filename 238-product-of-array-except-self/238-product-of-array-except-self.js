847c37e9862e9cc4007d40e0a3893a4fe5a92470
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-06-02 19:40:52 -0500

    Time: 691 ms (7.73%), Space: 58 MB (7.35%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.js
+++ 238-product-of-array-except-self/238-product-of-array-except-self.js
@@ -2,27 +2,32 @@
  * @param {number[]} nums
  * @return {number[]}
  */
+// inputs: array of integers
+// output: array of integers
+// constraints: array length > 1 | time complexity is O(n) | no division operator
+// edge cases: na
 var productExceptSelf = function(nums) {
+    let result = [];
+    // generate prefix and suffix arrays
     let prefix = [];
     let suffix = [];
-    // • create prefix array
     for (let i = 0; i < nums.length; i++) {
-        prefix.push(nums[i] * (prefix[i-1] !== undefined ? prefix[i - 1] : 1)); 
+        // prefix.push((nums[i - 1] || 0) * prefix[i - 1] || 1);
+        // suffix.unshift((nums[nums.length - i] || 0) * suffix[0] || 1);
+        if (prefix.length === 0) {
+            prefix.push(nums[i]);
+            suffix.push(nums[nums.length - 1]);
+        } else {
+            prefix.push(nums[i] * prefix[i - 1])
+            suffix.unshift(nums[nums.length - 1 - i] * suffix[0]);
+        }
     }
-    //  - loop through input
-    //  - starting with one, multiply
-    //    current itn. with prev el
-    // • create suffix array
-    nums.reverse();
-    for (let i = 0; i < nums.length; i++){
-        suffix.push(nums[i] * (suffix[i-1] !== undefined ? suffix[i - 1] : 1));
-    }
-    suffix.reverse();
-    //  - reverse loop thru input
-    //  - starting w/ one, multiply
-    //    current itn. w/ prv el
-    // • loop thru input, push product of prefix & suffix at i
+    // create a new array of the sums of prefix[i] & suffx[i]
+    //return array
     console.log(prefix, suffix)
-    return nums.map((num, i) => (prefix[i- 1] !== undefined ? prefix[i - 1] : 1) * (suffix[i + 1] !== undefined ? suffix[i + 1] : 1));
-    // • return
+    return nums.map((num, i) => {
+        return (prefix[i - 1] !== undefined ? prefix[i - 1] : 1) * (suffix[i + 1] !== undefined ? suffix[i + 1] : 1)
+    })
 };
+// [0, 1, 2, 6]
+// [24, 12, 4, 0]
