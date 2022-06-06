34f1874afd5318d930822b3a429533f510d351af
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-06-06 12:46:48 -0500

    Time: 79 ms (52.42%), Space: 42 MB (70.81%) - LeetHub

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let l = 0;
+    let r = nums.length - 1;
+    
+    while (l <= r) {
+        let m = Math.floor((l + r) / 2);
+        
+        if (target === nums[m]) return m;
+        
+        if (nums[l] <= nums[m]) {
+            if (target > nums[m] || target < nums[l]) {
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        } else {
+            if (target < nums[m] || target > nums[r]) {
+                r = m - 1;
+            } else {
+                l = m + 1;
+            }
+        }
+    }
+    return -1;
+};
+// [4,5,6,7,0,1,2]
+// 1 
