217a31d44b52f6dd923e2af962ffcdea96a7542b
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-06-06 11:35:11 -0500

    Time: 60 ms (93.94%), Space: 41.9 MB (81.94%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.js
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.js
@@ -3,21 +3,36 @@
  * @param {number} target
  * @return {boolean}
  */
-var searchMatrix = function(matrix, target) {
-    let flattened = matrix.flat();
-    
+var search = function(nums, target) {
     let l = 0;
-    let r = flattened.length - 1;
+    let r = nums.length - 1;
     
     while (l <= r) {
         let m = Math.floor((l + r) / 2);
         
-        if (flattened[m] < target) {
+        if (nums[m] < target) {
             l = m + 1;
-        } else if (flattened[m] > target) {
+        } else if (nums[m] > target) {
             r = m - 1;
         } else {
             return true;
+        }
+    }
+    return false;
+}
+var searchMatrix = function(matrix, target) {
+    let l = 0;
+    let r = matrix.length - 1;
+    
+    while (l <= r) {
+        let m = Math.floor((l + r) / 2);
+        
+        if (matrix[m][matrix[m].length - 1] < target) {
+            l = m + 1;
+        } else if (matrix[m][0] > target) {
+            r = m - 1;
+        } else {
+            return search(matrix[m], target);
         }
     }
     return false;
