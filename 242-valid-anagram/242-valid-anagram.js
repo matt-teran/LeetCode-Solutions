7a9040885858d6b95fc97689518131b229824753
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-06-06 21:54:44 -0500

    Time: 108 ms (56.15%), Space: 42.2 MB (97.73%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -9,20 +9,16 @@
 // constraints: string lengths > 0 | all lowercase letters
 // edge cases: na
 var isAnagram = function(s, t) {
-    //compare lengths, if not same, return false
     if (s.length !== t.length) return false;
-    // create a hashmap of the freq of characters in both strings
-    let sMap = {};
-    let tMap = {};
+    let countS = new Array(26).fill(0);
+    let countT = new Array(26).fill(0);
+    
     for (let i = 0; i < s.length; i++) {
-        s[i] in sMap ? sMap[s[i]]++ : sMap[s[i]] = 1;
-        t[i] in tMap ? tMap[t[i]]++ : tMap[t[i]] = 1;
+        countS[s[i].charCodeAt() - 97]++
+        countT[t[i].charCodeAt() - 97]++
     }
-    // compare hashmaps, if any properties are different, return false
-    for (const [k, v] of Object.entries(sMap)) {
-        if (!(k in tMap) || v !== tMap[k]) {
-            return false;
-        }
+    for (let i = 0; i < countS.length; i++) {
+        if (countS[i] !== countT[i]) return false;
     }
     return true;
-};
+}
