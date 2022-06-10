5c6059dbbfe1a27750df929f852a8e30bb8510b0
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-06-10 17:10:14 -0500

    Time: 152 ms (90.53%), Space: 65.4 MB (86.45%) - LeetHub

---
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var goodNodes = function(root) {
+    if (!root) return 0;
+    let result = 0;
+    
+    // let max = root.val;
+    
+    const dfs = (node, max) => {
+        if (!node) return;
+        
+        if (node.val >= max) {
+            max = node.val;
+            result++;
+        }
+        
+        if (node.left) dfs(node.left, max);
+        if (node.right) dfs(node.right, max)
+    }
+    dfs(root, root.val);
+    
+    return result;
+};
