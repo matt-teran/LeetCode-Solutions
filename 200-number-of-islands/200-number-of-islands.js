d1ecc0d9077817b945b3234223ea5a10ce175395
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-06-10 23:12:29 -0500

    Time: 246 ms (7.99%), Space: 64.2 MB (5.22%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.js
+++ 200-number-of-islands/200-number-of-islands.js
@@ -4,28 +4,30 @@
  */
 var numIslands = function(grid) {
     if (!grid.length) return 0;
+    
     let visited = new Set();
-    let [rows, cols] = [grid.length, grid[0].length];
+    
+    let rows = grid.length;
+    let cols = grid[0].length;
+    
     let islands = 0;
     
     const bfs = (row, col) => {
         let q = [[row, col]];
-        visited.add(JSON.stringify([row, col]));
+        visited.add(`r${row}c${col}`)
         
         while (q.length) {
-            let directions = [[1, 0], [-1, 0], [0, 1], [0, -1]];
-            let [row, col] = q.shift();
-            
+            [row, col] = q.shift();
+            const directions = [[1, 0], [-1, 0], [0,1], [0,-1]];
             for (let [dr, dc] of directions) {
-                let r = row + dr;
-                let c = col + dc;
+                let [r, c] = [row + dr, col + dc];
                 
                 if (0 <= r && r < rows &&
-                    0 <= c && c < cols &&
-                    grid[r][c] === '1' &&
-                    !visited.has(JSON.stringify([r, c]))) {
+                   0 <= c && c < cols &&
+                   grid[r][c] === '1' &&
+                   !visited.has(`r${r}c${c}`)) {
+                    visited.add(`r${r}c${c}`);
                     q.push([r, c]);
-                    visited.add(JSON.stringify([r, c]));
                 }
             }
         }
@@ -33,11 +35,12 @@ var numIslands = function(grid) {
     
     for (let r = 0; r < rows; r++) {
         for (let c = 0; c < cols; c++) {
-            if (grid[r][c] === '1' && !visited.has(JSON.stringify([r, c]))) {
+            if (grid[r][c] === '1' && !visited.has(`r${r}c${c}`)) {
                 bfs(r, c);
                 islands++;
             }
         }
     }
+    
     return islands;
 };
