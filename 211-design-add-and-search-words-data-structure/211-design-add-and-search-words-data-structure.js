e725cf3f5b6b6bbd2e044b0af9a07b1146ff0ca4
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-06-13 15:35:17 -0500

    Time: 4056 ms (17.71%), Space: 116.4 MB (43.79%) - LeetHub

---
+++ 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.js
@@ -0,0 +1,55 @@
+// WordDictionary Node
+var Node = function() {
+    this.children = {};
+    this.endOfWord = false;
+}
+var WordDictionary = function() {
+    this.root = new Node();
+};
+
+/** 
+ * @param {string} word
+ * @return {void}
+ */
+WordDictionary.prototype.addWord = function(word) {
+    let node = this.root;
+    for (let char of word) {
+        // if (char in node.children) {
+        //     node = node.children[char];
+        // } else {
+            node.children[char] = node.children[char] || new Node();
+            node = node.children[char];
+        // }
+    }
+    node.endOfWord = true;
+};
+
+/** 
+ * @param {string} word
+ * @return {boolean}
+ */
+WordDictionary.prototype.search = function(word, start=0, node=this.root) {
+    if (start === word.length) return node.endOfWord;
+    for (let i = start; i < word.length; i++) {
+        if (word[i] === '.') {
+            for (let key in node.children) {
+                if (this.search(word, i + 1, node.children[key])) return true;
+            }
+            return false;
+        } else {
+            if (word[i] in node.children) {
+                node = node.children[word[i]];
+            } else {
+                return false;
+            }    
+        }
+    }
+    return node.endOfWord;
+};
+
+/** 
+ * Your WordDictionary object will be instantiated and called as such:
+ * var obj = new WordDictionary()
+ * obj.addWord(word)
+ * var param_2 = obj.search(word)
+ */
