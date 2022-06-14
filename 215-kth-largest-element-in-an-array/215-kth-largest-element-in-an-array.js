4c67f8853c3f06fe63aaa62890f4c2d6b3ae2430
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-06-14 03:31:27 -0500

    Time: 135 ms (31.46%), Space: 46.6 MB (13.97%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.js
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.js
@@ -4,43 +4,10 @@
  * @return {number}
  */
 var findKthLargest = function(nums, k) {
-    if (nums.length === 1) return nums[0];
-    // reassign k to be the index of the kth largest element in a sorted array
-    k = nums.length - k;
-    // use quick select alg to find kth largest el
-    
-    const quickSelect = (l, r) => {
-        // set pointer to left most el
-        let pointer = l;
-        // set pivot to right most el
-        let pivot = nums[r];
-        // loop through array
-        for (let i = l; i < r; i++) {
-            // if itn. is less than pivot, swap pointer and itn. vals && increment pointer
-            if (nums[i] <= pivot) {
-                let swap = nums[pointer];
-                nums[pointer] = nums[i];
-                nums[i] = swap;
-                pointer++;
-            }
-        }
-        // after loop terminates => swap pivot w/ pointer
-        let swap = nums[pointer];
-        nums[pointer] = nums[r];
-        nums[r] = swap;
-        
-        // if k value is less than pivot value => repeat quick select on (l, pivot - 1)
-        if (pointer > k) {
-            return quickSelect(l, pointer - 1);
-        } else if (pointer < k) {
-            // else if k value is greater than pivot value => repeat (pivot + 1, r)
-            return quickSelect(pointer + 1, r);
-        } else {
-            // else return pivot value
-            console.log(pointer);
-            return nums[pointer];
-        }
-        
-    }
-    return quickSelect(0, nums.length - 1);
+    let heap = new MinPriorityQueue();
+    for (let num of nums) heap.enqueue(num);
+    while (heap.size() > k) {
+        heap.dequeue();
+    } 
+    return heap.front().element;
 };
