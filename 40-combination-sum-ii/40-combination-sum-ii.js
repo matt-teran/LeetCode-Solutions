01fe2e36d3f70d7a1d21f46ed1bfecf0439fe704
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-06-14 23:22:43 -0500

    Time: 139 ms (12.16%), Space: 45.9 MB (26.70%) - LeetHub

---
+++ 40-combination-sum-ii/40-combination-sum-ii.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum2 = function(candidates, target) {
+    const result = [];
+    candidates.sort((a, b) => a - b);
+    console.log(candidates);
+    const dfs = (i, combo, sum) => {
+        if (sum === target) return result.push([...combo]);
+        if (i === candidates.length) {
+            
+            return;
+        };
+        if (sum > target) return;
+        
+        combo.push(candidates[i]);
+        dfs(i + 1, combo, sum + candidates[i]);
+        
+        combo.pop();
+        while (i + 1 < candidates.length && candidates[i] === candidates[i + 1]) i++;
+        dfs(i + 1, combo, sum);
+    }
+    
+    dfs(0, [], 0);
+    return result;
+};
