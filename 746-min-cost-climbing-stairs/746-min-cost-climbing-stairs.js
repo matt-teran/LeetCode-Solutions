93c85575a4b9f8be72d937cbffe88fca28d7beb5
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-06-17 15:04:32 -0500

    Time: 86 ms (61.31%), Space: 44.1 MB (39.20%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.js
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.js
@@ -4,10 +4,12 @@
  */
 var minCostClimbingStairs = function(cost) {
     cost.push(0);
-    
-    for (let i = cost.length - 3; i >= 0; i--) {
+    // let i = cost.length - 1;
+//     while (i > -1) {
+        
+//     }
+    for (let i = cost.length - 3; i > -1; i--) {
         cost[i] += Math.min(cost[i + 1], cost[i + 2]);
     }
-    
     return Math.min(cost[0], cost[1]);
 };
