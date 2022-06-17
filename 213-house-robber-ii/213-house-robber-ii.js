08f7aa792b5fd55e7e819fc85e54b8cdd4088431
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-06-17 16:46:06 -0500

    Time: 72 ms (68.38%), Space: 42.4 MB (22.89%) - LeetHub

---
+++ 213-house-robber-ii/213-house-robber-ii.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums) {
+    if (!nums.length) return 0;
+    if (nums.length === 1) return nums[0];
+    let nums2 = [...nums];
+    nums.pop();
+    nums.push(0);
+    nums2.shift();
+    nums2.push(0);
+    
+    // DP table calculations
+    for (let i = nums.length - 3; i > -1; i--) {
+        nums[i] = Math.max(nums[i + 1], nums[i + 2] + nums[i]);
+        nums2[i] = Math.max(nums2[i + 1], nums2[i + 2] + nums2[i]);
+    }
+    return Math.max(nums[0], nums2[0]);
+};
