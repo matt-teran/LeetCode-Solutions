6592daf66eacb81af89f8d797d9f7284f16af37f
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-06-18 16:43:45 -0500

    Time: 65 ms (84.11%), Space: 42.1 MB (30.10%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.js
+++ 70-climbing-stairs/70-climbing-stairs.js
@@ -2,12 +2,19 @@
  * @param {number} n
  * @return {number}
  */
+
+const memo = {};
+
 var climbStairs = function(n) {
-    let [f1, f2] = [0, 1];
+    // recurrence relation - dp(n) = dp(n - 1) + dp(n - 2)
+    // base case
+    // function or data structure to caluclate or contain the answer at n
     
-    while (n > -1) {
-        [f1, f2] = [f1 + f2, f1];
-        n--;
+    if (n < 3) return n;
+    
+    if ( !(n in memo) ) {
+        memo[n] = climbStairs(n - 1) + climbStairs(n - 2);
     }
-    return f1;
+    
+    return memo[n];
 };
