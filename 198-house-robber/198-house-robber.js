8f378bae7f2830d49ebf2be95ddc20bce3184ef2
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-06-18 17:11:31 -0500

    Time: 63 ms (89.88%), Space: 41.9 MB (68.41%) - LeetHub

--- 198-house-robber/198-house-robber.js
+++ 198-house-robber/198-house-robber.js
@@ -2,17 +2,31 @@
  * @param {number[]} nums
  * @return {number}
  */
-
-// take the max value of current profit, plus the ith house or i + 1th house
+// const memo = {};
+//function
 var rob = function(nums) {
+    // recurrence relation
     
-    
-    if (!nums.length) return 0;
-    nums.push(0);
-    
-    // DP table calculations
-    for (let i = nums.length - 3; i > -1; i--){
-        nums[i] = Math.max(nums[i+1], nums[i+2] + nums[i]);
-    }
-    return nums[0];
+    function dp(i) {
+		// Base cases
+		if (i === 0) return nums[0];
+		if (i === 1) return Math.max(nums[0], nums[1]);
+ 
+		if ( !(i in memo) ) {
+			memo[i] = Math.max(dp(i - 1), dp(i - 2) + nums[i]) // Recurrance relation
+        }
+		return memo[i];
+	}
+	const memo = {};
+ 
+	return dp(nums.length - 1);
+
+        
+    // return dp(nums.length - 1);
 };
+
+// [2,7,9,3,1]
+// [2,7,9,3]
+// [2,7,9]
+// [2,7]
+// [2]
