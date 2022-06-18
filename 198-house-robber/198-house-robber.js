a36150a904f20356a6660e98cceaa1c6867d6caf
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-06-18 17:12:38 -0500

    Time: 86 ms (41.56%), Space: 42.1 MB (46.20%) - LeetHub

--- 198-house-robber/198-house-robber.js
+++ 198-house-robber/198-house-robber.js
@@ -2,27 +2,22 @@
  * @param {number[]} nums
  * @return {number}
  */
-// const memo = {};
+
 //function
 var rob = function(nums) {
     // recurrence relation
     
-    function dp(i) {
-		// Base cases
-		if (i === 0) return nums[0];
-		if (i === 1) return Math.max(nums[0], nums[1]);
- 
-		if ( !(i in memo) ) {
-			memo[i] = Math.max(dp(i - 1), dp(i - 2) + nums[i]) // Recurrance relation
-        }
-		return memo[i];
-	}
-	const memo = {};
- 
-	return dp(nums.length - 1);
-
+    const dp = (i) => {
+        // base case
+        if (i === 0) return nums[0];
+        if (i === 1) return Math.max(nums[0], nums[1]);
         
-    // return dp(nums.length - 1);
+        if ( !(i in memo) ) memo[i] = Math.max(dp(i - 1), dp(i - 2) + nums[i]);
+        return memo[i];
+    }
+    const memo = {};
+        
+    return dp(nums.length - 1);
 };
 
 // [2,7,9,3,1]
