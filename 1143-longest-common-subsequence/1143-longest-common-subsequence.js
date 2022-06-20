97802ecba69a6ce245f7c1f5e044d46b6f2b5c0e
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-06-20 15:04:01 -0500

    Time: 131 ms (63.65%), Space: 52.2 MB (85.65%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.js
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.js
@@ -6,21 +6,34 @@
 var longestCommonSubsequence = function(text1, text2) {
     if (text2.length > text1.length) [text1, text2] = [text2, text1];
     const memo = {};
+    const dp = [];
+    for (let i = 0; i <= text1.length + 1; i++) {
+        dp.push(new Array(text2.length + 1).fill(0));
+    }
     
-    const dp = (i, j) => {
-        if (i === text1.length) return 0;
-        if (j === text2.length) return 0;
-        let res = text1[i] === text2[j] ? 1 : 0;
-        if ( !(i in memo) ) memo[i] = {};
-        
-        if ( !(j in memo[i]) ) {
-            memo[i][j] = Math.max(res + dp(i+1, j+1), dp(i+1, j), dp(i, j+1));
+    for (let i = text1.length-1; i >= 0; i--) {
+        for (let j = text2.length-1; j >= 0; j--) {
+            let res = text1[i] === text2[j] ? 1 : 0;
+            dp[i][j] = Math.max(res + dp[i+1][j+1],
+                                dp[i+1][j],
+                                dp[i][j+1]);
         }
-        
-        return memo[i][j];
-    };
+    }
     
-    return dp(0,0);
+//     const dp = (i, j) => {
+//         if (i === text1.length) return 0;
+//         if (j === text2.length) return 0;
+//         let res = text1[i] === text2[j] ? 1 : 0;
+//         if ( !(i in memo) ) memo[i] = {};
+        
+//         if ( !(j in memo[i]) ) {
+//             memo[i][j] = Math.max(res + dp(i+1, j+1), dp(i+1, j), dp(i, j+1));
+//         }
+        
+//         return memo[i][j];
+//     };
+    
+    return dp[0][0];
 };
 
 // base case
