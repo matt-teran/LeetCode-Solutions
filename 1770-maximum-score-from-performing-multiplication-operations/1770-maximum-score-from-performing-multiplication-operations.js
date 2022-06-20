10ecba23456549b5d7b5613246083fc8b9d6d135
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-06-20 00:48:13 -0500

    Time: 1325 ms (19.10%), Space: 87.4 MB (33.15%) - LeetHub

---
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @param {number[]} multipliers
+ * @return {number}
+ */
+var maximumScore = function(nums, multipliers) {
+    let n = nums.length;
+    let m = multipliers.length;
+    const memo = {};
+    const dp = (i, left) => {
+        if (i === m) return 0;
+        let mult = multipliers[i];
+        let right = n - 1 - (i - left);
+        if ( !(i in memo) ) {
+            memo[i] = {};
+            memo[i][left] = Math.max(mult * nums[left] + dp(i + 1, left + 1),
+                        mult * nums[right] + dp(i + 1, left));
+        } else if ( !(left in memo[i]) ){
+            memo[i][left] = Math.max(mult * nums[left] + dp(i + 1, left + 1),
+                        mult * nums[right] + dp(i + 1, left));
+        }
+        
+        return memo[i][left];
+    }
+    
+    return dp(0, 0);
+};
