e0dcc65839c9f3d61b10700be65aa4a67349a632
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-06-20 14:20:49 -0500

    Time: 392 ms (85.39%), Space: 85.6 MB (70.79%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.js
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.js
@@ -6,21 +6,21 @@
 var maximumScore = function(nums, multipliers) {
     let n = nums.length;
     let m = multipliers.length;
-    const memo = {};
-    const dp = (i, left) => {
-        if (i === m) return 0;
-        let mult = multipliers[i];
-        let right = n - 1 - (i - left);
-        
-        if ( !(i in memo) ) memo[i] = {};
-        
-        if ( !(left in memo[i]) ){
-            memo[i][left] = Math.max(mult * nums[left] + dp(i + 1, left + 1),
-                        mult * nums[right] + dp(i + 1, left));
+    
+    let tabularDP = [];
+    for (let i = 0; i <= m; i++) {
+        tabularDP.push(new Array(m + 1).fill(0));
+    }
+
+    // tabularDP[m][m] = 0;
+    for (let i = m - 1; i >= 0; i--){
+        for (let left = i; left >= 0; left--) {
+            let mult = multipliers[i];
+            let right = n - 1 - (i - left);
+            tabularDP[i][left] = Math.max(mult * nums[left] + tabularDP[i + 1][left + 1],
+                        mult * nums[right] + tabularDP[i + 1][left]);
         }
-        
-        return memo[i][left];
     }
     
-    return dp(0, 0);
+    return tabularDP[0][0];
 };
