b9c3a1c9e3367c54dff4defbc7de42a893dba1d2
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-06-19 22:58:55 -0500

    Time: 77 ms (61.85%), Space: 42.4 MB (26.66%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.js
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.js
@@ -3,12 +3,5 @@
  * @return {number}
  */
 var maximumWealth = function(accounts) {
-    let largest = accounts[0].reduce((acc, x) => acc + x, 0);
-    
-    for (let i = 1; i < accounts.length; i++) {
-        let sum = accounts[i].reduce((acc, x) => acc + x, 0);
-        if (sum > largest) largest = sum;
-    }
-    
-    return largest;
+    return Math.max(...accounts.map(user => user.reduce((acc, bank) => acc + bank, 0)))
 };
