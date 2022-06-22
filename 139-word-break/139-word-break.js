3b98c10d44ebd863bd227f8da23088d5c9c10d38
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-06-21 21:42:53 -0500

    Time: 103 ms (40.11%), Space: 41.8 MB (99.91%) - LeetHub

--- 139-word-break/139-word-break.js
+++ 139-word-break/139-word-break.js
@@ -3,26 +3,32 @@
  * @param {string[]} wordDict
  * @return {boolean}
  */
-var wordBreak = function(s, wordDict) {
-    const memo = {};
-    
-    const dp = (cs) => {
-        if (cs === s) return true;
-        if (cs !== s.slice(0, cs.length)) return false;
-        if (cs.length > s.length) return false;
+// class Solution:
+//     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+//         dp = [False] * len(s)
+//         for i in range(len(s)):
+//             for word in wordDict:
+//                 # Make sure to stay in bounds while checking criteria
+//                 if i >= len(word) - 1 and (i == len(word) - 1 or dp[i - len(word)]):
+//                     if s[i - len(word) + 1 : i + 1] == word:
+//                         dp[i] = True
+//                         break
         
-        if ( !(cs in memo) ) {
-            let bool = false;
-            for (let word of wordDict) {
-                if (dp(cs.concat(word))) bool = true;
+//         return dp[-1]
+var wordBreak = function(s, wordDict) {
+    const dp = [];
+    for (let i = 0; i < s.length; i++) dp.push(false);
+    for (let i = 0; i < s.length; i++) {
+        for (let word of wordDict) {
+            if (i >= word.length - 1 && (i === word.length -1 || dp[i - word.length])) {
+                if (s.slice(i - word.length + 1, i + 1) === word) {
+                    dp[i] = true;
+                    break;
+                }
             }
-            memo[cs] = bool;
         }
-            
-        return memo[cs];
     }
-    
-    return dp('');
+    return dp.at(-1);
 };
 
 // base case
