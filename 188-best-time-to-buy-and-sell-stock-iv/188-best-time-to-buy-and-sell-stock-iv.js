28e1ffbf242ec0d4bd0fd52e55fc66174b02becc
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-06-22 18:35:37 -0500

    Time: 182 ms (34.06%), Space: 83.3 MB (5.68%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.js
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.js
@@ -3,46 +3,29 @@
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = function(k, prices) {
-    if (prices.length === 0 || k === 0) return 0;
-
-    const memo = {};
-    
-    const dp = (i, t) => {
-        if (t === k) return 0;
-        if (prices.length - 1 === i) {
-            return t % 1 ? prices.at(-1) : 0;
-        };
-        
-        if ( !(i in memo) ) memo[i] = {};
-        if ( !(t in memo[i]) ) {
-            const additive = t % 1 ? prices[i] : -prices[i];
-            memo[i][t] = Math.max(dp(i + 1, t + 0.5) + additive, dp(i + 1, t));
-        }
-        
-        return memo[i][t];
-        
-        
-        
-        // return Math.max(dp(i + 1, t + 0.5) + additive, dp(i + 1, t));
-    }
-    
-    return dp(0, 0);
-};
-
-// dp function
-// dp(i, t) => max profit for n prices and k transactions
-
-// recurrence relation
-// dp(i, t) === Max of:
-// • dp(i + 1, t + 0.5) + (prices[i] if selling, -prices[i] if buying)
-// • dp(i + 1, t);
-
-// base case
-// • if only one price remains && not holding => return 0
-// • all transactions made => return 0
-
-// state variables
-// • # number of transactions
-// • index of price
-// • profit
+var maxProfit = (k, prices) => {
+	const memo = {};
+	const dp = (i, transactionsRemaining, holding) => {
+		// Base case
+		if (transactionsRemaining === 0 || i === prices.length) return 0;
+ 
+		if ( !(i in memo) ) memo[i] = {};
+		if ( !(transactionsRemaining in memo[i]) ) memo[i][transactionsRemaining] = {};
+		if ( !(holding in memo[i][transactionsRemaining]) ) {
+			let doNothing = dp(i + 1, transactionsRemaining, holding);
+			let doSomething = 0;
+ 
+		if (holding) {
+				// Sell stock
+				doSomething = prices[i] + dp(i + 1, transactionsRemaining - 1, 0);
+		} else {
+				// Buy stock
+				doSomething = -prices[i] + dp(i + 1, transactionsRemaining, 1);
+		}
+		// Recurrence relation
+			memo[i][transactionsRemaining][holding] = Math.max(doNothing, doSomething);
+		}
+		return memo[i][transactionsRemaining][holding];
+	}
+	return dp(0, k, 0);
+}
