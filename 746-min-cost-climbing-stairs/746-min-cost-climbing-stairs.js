5914ef8cd4f1add82de5f3ceb56cb506344719d5
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-06-22 22:53:50 -0500

    Time: 72 ms (86.30%), Space: 45.6 MB (5.79%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.js
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.js
@@ -3,13 +3,13 @@
  * @return {number}
  */
 var minCostClimbingStairs = function(cost) {
-    const memo = {};
     cost.push(0);
-    const dp = (i) => {
-        if (i < 2) return cost[i]; // base case
-        
-        if ( !(i in memo) ) memo[i] = cost[i] + Math.min(dp(i-1), dp(i-2)); // recurrence relation
-        return memo[i]
+    let minusOne = cost[1];
+    let minusTwo = cost[0];
+    
+    for (let i = 2; i < cost.length; i++) {
+        [minusOne, minusTwo] = [cost[i] + Math.min(minusOne, minusTwo), minusOne];
     }
-    return dp(cost.length - 1);
+
+    return minusOne;
 };
