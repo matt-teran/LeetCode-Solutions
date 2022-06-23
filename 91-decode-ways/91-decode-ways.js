085a8f3de572dcabb208a603e2b47fdc012b41cc
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-06-22 20:50:28 -0500

    Time: 75 ms (81.12%), Space: 41.9 MB (92.52%) - LeetHub

--- 91-decode-ways/91-decode-ways.js
+++ 91-decode-ways/91-decode-ways.js
@@ -3,23 +3,35 @@
  * @return {number}
  */
 var numDecodings = function(s) {
-    const memo = {};
+    const n = s.length
     
-    const dp = (i) => {
-        if (i >= s.length) return 1;
-        // if (s[i] === "0" || s.slice(i, i+2) > 26) return 0;
-        
-        if ( !(i in memo) ) {
-            memo[i] = 0;
-            if (s[i] !== "0") {
-                memo[i] += dp(i+1);
-                if (i < s.length - 1 && s.slice(i, i+2) < 27) memo[i] += dp(i+2);
-            }
+    const dp = new Array(n + 1).fill(0);
+    dp.fill(1, n);
+    
+    for (let i = n - 1; i > -1; i--) {
+        if (s[i] !== "0") {
+            dp[i] += dp[i+1];
+            if (i < s.length - 1 && s.slice(i, i+2) < 27) dp[i] += dp[i+2];
         }
-        
-        return memo[i]
     }
-    return dp(0);
+    return dp[0];
+    
+//     const memo = {};
+    
+//     const dp = (i) => {
+//         if (i >= s.length) return 1;
+        
+//         if ( !(i in memo) ) {
+//             memo[i] = 0;
+//             if (s[i] !== "0") {
+//                 memo[i] += dp(i+1);
+//                 if (i < s.length - 1 && s.slice(i, i+2) < 27) memo[i] += dp(i+2);
+//             }
+//         }
+        
+//         return memo[i]
+//     }
+//     return dp(0);
 };
 
 // base case
