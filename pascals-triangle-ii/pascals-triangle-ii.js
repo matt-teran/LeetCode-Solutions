dc82fddd1a611291334c2ee4fad44ea18ea3365d
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-06-23 17:03:13 -0500

    Time: 105 ms, Memory: 42.1 MB - LeetHub

--- pascals-triangle-ii/pascals-triangle-ii.js
+++ pascals-triangle-ii/pascals-triangle-ii.js
@@ -5,11 +5,13 @@
 var getRow = function(rowIndex) {
     if (rowIndex === 0) return [1];
     if (rowIndex === 1) return [1,1];
-    let gotRow = getRow(rowIndex-1);
-    let res = [1];
-    for (let i = 0; i < gotRow.length - 1; i++) {
-        res.push(gotRow[i]+gotRow[i+1]);
-    }
-    res.push(1);
-    return res;
+    
+    return getRow(rowIndex-1).reduce((acc, cur, i, arr) => {
+        if (i === arr.length -1) {
+            acc.push(1);
+            return acc;
+        };
+        acc.push(cur+arr[i+1]);
+        return acc;
+    }, [1])
 };
