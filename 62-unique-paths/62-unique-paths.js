970540b92e5134c10f33830f87702ad3c0bc3dae
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-06-23 01:15:47 -0500

    Time: 77 ms (66.25%), Space: 42.1 MB (60.73%) - LeetHub

--- 62-unique-paths/62-unique-paths.js
+++ 62-unique-paths/62-unique-paths.js
@@ -3,34 +3,26 @@
  * @param {number} n
  * @return {number}
  */
+var factorialize = function (num) {
+    var result = 1;
+    for (var i = 2; i <= num; i++) result *= i;
+    return result;
+}
+
 var uniquePaths = function(m, n) {
-    const memo = {};
+    return factorialize(m+n-2)/(factorialize(m-1) * factorialize(n-1));
     
-    const dp = [];
-    for (let i = 0; i <= m; i++) {
-        dp.push(new Array(n+1).fill(1));
-    }
-    dp[m-1][n-1] = 1;
-    
-    for (let r = m-2; r >= 0; r--) {
-        for (let c = n - 2; c >= 0; c--){
-            dp[r][c] = dp[r+1][c] + dp[r][c+1];
-        }
-    }
-    // console.log(dp[0][0]);
-    return dp[0][0];
-    
-//     const dp = (i, j) => {
-        
-//         if (i > m || j > n) return 0;
-//         if (i === m-1 && j === n-1)return 1;
-        
-//         if ( !memo.hasOwnProperty(i) ) memo[i] = {};
-//         if ( !(j in memo[i]) ) {
-//             memo[i][j] = dp(i + 1, j) + dp(i, j + 1);
-//         }
-//         return memo[i][j]
+//     const dp = [];
+//     for (let i = 0; i <= m; i++) {
+//         dp.push(new Array(n+1).fill(1));
 //     }
+//     dp[m-1][n-1] = 1;
     
-//     return dp(0, 0);
+//     for (let r = m-2; r >= 0; r--) {
+//         for (let c = n - 2; c >= 0; c--){
+//             dp[r][c] = dp[r+1][c] + dp[r][c+1];
+//         }
+//     }
+
+//     return dp[0][0];
 };
