ec9bdb06fbc7681dbada16144f24b51c15b7b1d2
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-06-25 15:21:30 -0500

    Time: 92 ms (68.30%), Space: 45.4 MB (59.18%) - LeetHub

--- 261-graph-valid-tree/261-graph-valid-tree.js
+++ 261-graph-valid-tree/261-graph-valid-tree.js
@@ -4,28 +4,39 @@
  * @return {boolean}
  */
 var validTree = function(n, edges) {
+    console.log(n -1, edges.length)
+    if (edges.length !== n - 1) return false;
+    
+    // Create Adjacency List
     const adj = {};
-    const parent = new Map();
-    parent.set(0, -1);
-    for (let i = 0; i < n; i++) adj[i] = [];
+    for (let i = 0; i < n; i++) adj[i] = []; 
     for (let [x, y] of edges) {
         adj[x].push(y);
         adj[y].push(x);
     }
+    
+    // Create 'seen' map
+    // const seen = new Map();
+    // seen.set(0, -1);
+    const seen = new Set([0]);
 
+    // Perform Iterative DFS
     let stack = [0];
     while (stack.length) {
         let node = stack.pop();
         
         for (let neighbor of adj[node]) {
-            if (parent.get(node) === neighbor) continue;
+            // Check if target neighbor has already been seen
+            if (seen.has(neighbor)) continue;
             
-            if (parent.has(neighbor)) return false;
+            // If target node has been seen, return false
+            // if (seen.has(neighbor)) return false;
             
+            // Continue DFS, Add reverse relationship to seen
             stack.push(neighbor);
-            parent.set(neighbor, node);
+            seen.add(neighbor);
         }
     }
 
-    return parent.size === n;
+    return seen.size === n;
 };
