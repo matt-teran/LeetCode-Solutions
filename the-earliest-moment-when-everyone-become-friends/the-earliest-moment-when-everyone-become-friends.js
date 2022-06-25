16abd4e97849d1204e6543ac53fbddab838139d7
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-06-25 00:33:49 -0500

    Time: 94 ms, Memory: 47.9 MB - LeetHub

---
+++ the-earliest-moment-when-everyone-become-friends/the-earliest-moment-when-everyone-become-friends.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[][]} logs
+ * @param {number} n
+ * @return {number}
+ */
+var earliestAcq = function(logs, n) {
+    logs.sort((a,b)=>a[0]-b[0]);
+    const root = Array(n - 1);
+    for (let i=0;i<n;i++)root[i]=i;
+    const rank = Array(n).fill(1);
+    const find = x => x === root[x] ? x : root[x] = find(root[x]);
+    const union = (x, y) => {
+        let rootX = find(x);
+        let rootY = find(y);
+        
+        if (rootX === rootY) return;
+        if (rank[rootX] > rank[rootY]) {
+            root[rootY] = rootX;
+        } else if (rank[rootX] < rank[rootY]) {
+            root[rootX] = rootY;
+        } else {
+            root[rootY] = rootX;
+            rank[rootX]++;
+        }
+    }
+    
+    for (let [timestamp, x, y] of logs) {
+        union(x, y);
+        let friendGroups = new Set();
+        for (let i = 0; i < n; i++) {
+            friendGroups.add(find(i));
+        }
+        if (friendGroups.size === 1) return timestamp;
+    }
+    return -1;
+};
