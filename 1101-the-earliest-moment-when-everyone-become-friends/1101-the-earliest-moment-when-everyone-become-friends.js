ac930c8dd8e066d41f604ad00fa42be3c87ade87
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-06-25 17:49:13 -0500

    Time: 115 ms (37.63%), Space: 45.6 MB (26.34%) - LeetHub

--- 1101-the-earliest-moment-when-everyone-become-friends/1101-the-earliest-moment-when-everyone-become-friends.js
+++ 1101-the-earliest-moment-when-everyone-become-friends/1101-the-earliest-moment-when-everyone-become-friends.js
@@ -4,16 +4,18 @@
  * @return {number}
  */
 var earliestAcq = function(logs, n) {
-    logs.sort((a,b)=>a[0]-b[0]);
-    const root = Array(n - 1);
-    for (let i=0;i<n;i++)root[i]=i;
-    const rank = Array(n).fill(1);
+    logs.sort((a,b)=>a[0]-b[0]); // l * log l
+    const root = [];
+    for (let i=0;i<n;i++)root.push(i); // n
+    const rank = Array(n).fill(1); // 
     const find = x => x === root[x] ? x : root[x] = find(root[x]);
     const union = (x, y) => {
         let rootX = find(x);
         let rootY = find(y);
         
-        if (rootX === rootY) return true;
+        if (rootX === rootY) return;
+        n--
+        if (n === 1) return true;
         if (rank[rootX] > rank[rootY]) {
             root[rootY] = rootX;
         } else if (rank[rootX] < rank[rootY]) {
@@ -24,13 +26,6 @@ var earliestAcq = function(logs, n) {
         }
     }
     
-    for (let [timestamp, x, y] of logs) {
-        if (union(x, y)) continue;
-        let friendGroups = new Set();
-        for (let i = 0; i < n; i++) {
-            friendGroups.add(find(i));
-        }
-        if (friendGroups.size === 1) return timestamp;
-    }
-    return -1;
+    for (let [timestamp, x, y] of logs) if (union(x, y)) return timestamp
+    return -1; // n + l log l + l (a(l))
 };
