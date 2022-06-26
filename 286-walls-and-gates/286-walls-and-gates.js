0c3b4d116bed5281b289e5d18b3dfda1661113b3
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-06-26 15:18:30 -0500

    Time: 1152 ms (8.09%), Space: 49.7 MB (81.16%) - LeetHub

--- 286-walls-and-gates/286-walls-and-gates.js
+++ 286-walls-and-gates/286-walls-and-gates.js
@@ -5,10 +5,9 @@
 var wallsAndGates = function(rooms) {
     const ROWS = rooms.length;
     const COLS = rooms[0].length;
+    const DIR = [[1,0],[-1,0],[0,1],[0,-1]];
     const INF = 2147483647;
-    const DIR = [[1, 0], [-1, 0], [0, 1], [0, -1]];
     const q = [];
-    
     for (let r = 0; r < ROWS; r++) {
         for (let c = 0; c < COLS; c++) {
             if (rooms[r][c] === 0) q.push([r,c]);
@@ -21,8 +20,8 @@ var wallsAndGates = function(rooms) {
         let qLen = q.length;
         for (let i = 0; i < qLen; i++) {
             let [row, col] = q.shift();
-            for (let [dr, dc] of DIR) {
-                let [r, c] = [row + dr, col + dc];
+            for (let [dr,dc] of DIR) {
+                let [r,c] = [row+dr,col+dc];
                 if (rooms?.[r]?.[c] === INF) {
                     rooms[r][c] = distance;
                     q.push([r,c]);
