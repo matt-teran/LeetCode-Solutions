9e03ecec5779033557c3ffcf717567d3904c676a
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-06-26 15:00:39 -0500

    Time: 925 ms (7.60%), Space: 51 MB (13.58%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -2,26 +2,23 @@
  * @param {string} s
  * @return {number}
  */
-
-// inputs: string of chars (any)
-// output: integer (length of longest substring w/o dups)
-// constraints: na
-// edge cases: string length is 0
 var lengthOfLongestSubstring = function(s) {
-    if (s.length === 0) return 0;
+    let count = {}
     
-    // set two pointers, i @ 0, j @ 1
-    let i = 0;
-    let j = 0;
-    let hashset = new Set();
+    const isDup = () => {
+        for (let char of Object.values(count)) if (char > 1) return true;
+    }
+    
     let result = 0;
-    for (j; j < s.length; j++) {
-        while (hashset.has(s[j])) {
-            hashset.delete(s[i]);
-            i++;
+    let l = 0;
+    for (let r = 0; r < s.length; r++) {
+        count[s[r]] ? count[s[r]]++ : count[s[r]] = 1;
+        if(isDup()) {
+            count[s[l]]--;
+            l++
+        } else {
+            result++;
         }
-        hashset.add(s[j]);
-        result = Math.max(result, j - i + 1);
     }
     return result;
 };
