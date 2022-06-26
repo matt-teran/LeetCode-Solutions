4122a41eadf62fa8fa849ea769922f2c3ec07b40
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-06-26 15:39:59 -0500

    Time: 83 ms (50.63%), Space: 42.3 MB (31.63%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.js
+++ 213-house-robber-ii/213-house-robber-ii.js
@@ -4,26 +4,27 @@
  */
 var rob = function(nums) {
     if (nums.length === 1) return nums[0];
+    
     let nums1 = nums.slice(1);
-    let nums2 = nums.slice(0, nums.length-1);
-    const memo1 = {};
-    const memo2 = {};
-    const dp1 = (i) => {
-        if (i < 0) return 0;
-        if (!memo1.hasOwnProperty(i)) {
-            memo1[i] = Math.max(nums1[i] + dp1(i-2), dp1(i-1));
-        }
-        return memo1[i];
-    }
-    const dp2 = (i) => {
-        if (i < 0) return 0;
-        if (!memo2.hasOwnProperty(i)) {
-            memo2[i] = Math.max(nums2[i] + dp2(i-2), dp2(i-1));
-        }
-        return memo2[i];
-    }
-    return Math.max(dp1(nums1.length-1), dp2(nums2.length-1));
+    let nums2 = nums.slice(0, nums.length - 1);
+    
+    return Math.max(dpParent(nums1), dpParent(nums2));
 };
+
+var dpParent = (nums) => {
+    const memo = {};
+    const dp = (i) => {
+        if (i < 0) return 0;
+        
+        if (!memo.hasOwnProperty(i)) {
+            memo[i] = Math.max(nums[i] + dp(i-2), dp(i-1));
+        }
+        
+        return memo[i];
+    }
+    
+    return dp(nums.length-1);
+}
 
 // base case
 // • i <2
