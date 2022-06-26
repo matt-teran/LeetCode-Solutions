aea1cab8a555bd1dc126a56f73a6fb1fdac5f158
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-06-26 11:59:36 -0500

    Time: 520 ms (73.37%), Space: 42.6 MB (74.39%) - LeetHub

--- 79-word-search/79-word-search.js
+++ 79-word-search/79-word-search.js
@@ -7,26 +7,21 @@ var exist = function(board, word) {
     const ROWS = board.length;
     const COLS = board[0].length;
     
-    // const visited = new Set();
-    
-    const dfs = (r, c, i, visited) => {
+    const dfs = (r, c, i) => {
         if (i === word.length) return true;
         if (r < 0 || r >= ROWS ||
            c < 0 || c >= COLS ||
-            visited.has(`r${r}c${c}`) ||
            board[r][c] !== word[i]) return false;
-        visited.add(`r${r}c${c}`)
-        // console.log(visited);
-        const ans = (i+1 === word.length || dfs(r+1,c,i+1,visited) || dfs(r-1,c,i+1,visited) ||
-           dfs(r,c+1,i+1,visited) || dfs(r,c-1,i+1,visited))
-        visited.delete(`r${r}c${c}`);
+        board[r][c] = '#';
+        const ans = (dfs(r+1,c,i+1) || dfs(r-1,c,i+1) ||
+           dfs(r,c+1,i+1) || dfs(r,c-1,i+1))
+        board[r][c] = word[i];
         return ans;
     }
     
     for (let r = 0; r < ROWS; r++) {
         for (let c = 0; c < COLS; c++) {
-            // visited.clear();
-            if (board[r][c] === word[0] && dfs(r, c, 0, new Set())) return true;
+            if (board[r][c] === word[0] && dfs(r, c, 0)) return true;
         }
     }
     
