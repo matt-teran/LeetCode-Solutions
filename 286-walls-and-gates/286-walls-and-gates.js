becdbd0d15c4c984f56607f301146a3e7e5b5c13
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-06-26 13:00:30 -0500

    Time: 314 ms (29.91%), Space: 50.1 MB (63.30%) - LeetHub

--- 286-walls-and-gates/286-walls-and-gates.js
+++ 286-walls-and-gates/286-walls-and-gates.js
@@ -3,39 +3,31 @@
  * @return {void} Do not return anything, modify rooms in-place instead.
  */
 var wallsAndGates = function(rooms) {
+    const ROWS = rooms.length;
+    const COLS = rooms[0].length;
+    const INF = 2147483647;
+    const DIR = [[1, 0], [-1, 0], [0, 1], [0, -1]];
+    const q = [];
+    
+    for (let r = 0; r < ROWS; r++) {
+        for (let c = 0; c < COLS; c++) {
+            if (rooms[r][c] === 0) q.push([r,c]);
+        }
+    }
+    
     let distance = 0;
-    let rows = rooms.length;
-    let cols = rooms[0].length;
-    let gates = [];
-
-    const bfs = (gates) => {
-      let q = [...gates];
-      const directions = [[1,0],[-1,0],[0,1],[0,-1]];
-
-      while(q.length) {
+    while (q.length) {
         distance++;
         let qLen = q.length;
-
         for (let i = 0; i < qLen; i++) {
-          let [row, col] = q.shift();
-
-          for (let [dr, dc] of directions) {
-            let [r, c] = [row + dr, col + dc];
-
-            if (0 <= r && r < rows &&
-                0 <= c && c < cols &&
-                rooms[r][c] === 2147483647) {
-                  q.push([r,c]);
-                  rooms[r][c] = distance;
+            let [row, col] = q.shift();
+            for (let [dr, dc] of DIR) {
+                let [r, c] = [row + dr, col + dc];
+                if (rooms?.[r]?.[c] === INF) {
+                    rooms[r][c] = distance;
+                    q.push([r,c]);
                 }
-          }
+            }
         }
-      }
     }
-    for (let r = 0; r < rows; r++) {
-      for (let c = 0; c < cols; c++) {
-        if (rooms[r][c] === 0) gates.push([r, c]);
-      }
-    }
-    bfs(gates);
 };
