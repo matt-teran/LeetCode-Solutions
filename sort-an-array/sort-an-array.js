00b70307a96643765b1e016467d575bfdbb79a1c
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-06-27 15:57:19 -0500

    Time: 7995 ms, Memory: 60.9 MB - LeetHub

--- sort-an-array/sort-an-array.js
+++ sort-an-array/sort-an-array.js
@@ -3,33 +3,29 @@
  * @return {number[]}
  */
 var sortArray = function(nums) {
-    const mergeSort = (nums) => {
-        if (nums.length < 2) return nums;
-        let pivot = Math.floor(nums.length / 2);
-        let left = mergeSort(nums.slice(0,pivot));
-        let right = mergeSort(nums.slice(pivot));
-        
-        return merge(left, right);
+    const quickSort = (nums) => {
+        let n = nums.length;
+        qSort(nums, 0, n - 1);
     }
-    
-    const merge = (left, right) => {
-        const result = [];
-        let l = 0;
-        let r = 0;
-        while (l < left.length && r < right.length) {
-            if (left[l] < right[r]) {
-                result.push(left[l]);
-                l++;
-            } else {
-                result.push(right[r]);
-                r++;
+    const qSort = (nums, lo, hi) => {
+        if (lo < hi) {
+            let p = partition(nums, lo, hi);
+            qSort(nums, lo, p - 1);
+            qSort(nums, p + 1, hi);
+        }
+    }
+    const partition = (nums, lo, hi) => {
+        let pivot = nums[hi];
+        let i = lo;
+        for (let j = lo; j < hi; j++) {
+            if (nums[j] < pivot) {
+                [nums[i], nums[j]] = [nums[j], nums[i]];
+                i++;
             }
         }
-        result.push(...left.slice(l));
-        result.push(...right.slice(r));
-        
-        return result;
+        [nums[i], nums[hi]] = [nums[hi], nums[i]];
+        return i;
     }
-    
-    return mergeSort(nums);
+    quickSort(nums);
+    return nums;
 };
