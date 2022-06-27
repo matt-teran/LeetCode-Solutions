3eaa24b65c6b4abb6d21b8e2335b5ed9b8b0dba8
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-06-26 22:21:53 -0500

    Time: 92 ms (82.35%), Space: 48.5 MB (21.95%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.js
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.js
@@ -3,34 +3,13 @@
  * @param {number} k
  * @return {number[]}
  */
-
-// inputs: array, integer
-// output: array of k most frequesnt integers
-// constraints: nums length > 0 | k > 0
-// edge cases: na
 var topKFrequent = function(nums, k) {
-    // keep track of freq w/ hashmap
-    let map = {};
-    let freq = Array.from(Array(nums.length + 1), () => new Array());
-    for (let num of nums) {
-        map[num] ? map[num]++ : map[num] = 1;
-    }
-    // create array of k-v pairs/
-    // Object.entries(map).forEach(([n, c]) => {
-    //     freq[c].push(n);
-    // })
-    for (const [n, c] of Object.entries(map)) {
-        // console.log(freq);
-        freq[c].push(n)
-    }
-    // console.log(map, freq);
-    let result = [];
-    for (let i = freq.length - 1; i > -1; i--) {
-        for (let j = 0; j < freq[i].length; j++) {
-            result.push(freq[i][j]);
-            if (result.length === k) return result;
-        }
-    }
-};
+    const count = {};
+    const result = []
+    for (let num of nums) count[num] ? count[num]++ : count[num] = 1;
+    const utility = []
+    for (let [num, freq] of Object.entries(count)) utility.push([num, freq]);
+    utility.sort((a,b) => b[1] - a[1]);
 
-// O(3n * log n)
+    return utility.map(([num, freq]) => num).slice(0, k);
+};
