a4bddfe0720a16107e2dbe6368ae727ea68c176a
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-06-27 20:50:13 -0500

    Time: 98 ms, Memory: 49.7 MB - LeetHub

--- all-paths-from-source-to-target/all-paths-from-source-to-target.js
+++ all-paths-from-source-to-target/all-paths-from-source-to-target.js
@@ -7,15 +7,15 @@ var allPathsSourceTarget = function(graph) {
     const source = 0;
     const target = N - 1;
     const result = [];
-    const adj = {};
+    // const adj = {};
     const path = [0];
     const visited = new Set();
-    for (let i = 0; i < N; i++) adj[i] = graph[i];
+    // for (let i = 0; i < N; i++) adj[i] = graph[i];
     
     const dfs = (node) => {
         if (node === target) return result.push([...path]);
         
-        for (let neighbor of adj[node]) {
+        for (let neighbor of graph[node]) {
             if (!visited.has(neighbor)) {
                 path.push(neighbor);
                 visited.add(neighbor);
