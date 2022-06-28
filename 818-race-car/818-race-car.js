2d09fd084cb9162420e3e6e90f1a25e4408bb251
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-06-28 16:48:09 -0500

    Time: 4478 ms (5.01%), Space: 128.9 MB (5.01%) - LeetHub

--- 818-race-car/818-race-car.js
+++ 818-race-car/818-race-car.js
@@ -1,24 +1,28 @@
 const racecar = (target) => {
+    // create our initial state, we start at position 0 with speed 1
 	const initialState = [0,1];
-	const q = [initialState];
-	const visited = new Set();
-	visited.add(initialState.join('#'));
+    // initialize our BFS queue with this state
+	const q = [['', initialState]];
+    // initialize visited set w/ initial state
+	const visited = new Set([initialState.join('#')]);
 
 	for (let level = 0; q.length; level++) {
-        let qLen = q.length;
-		for (let k = qLen; k > 0; k--) {
-			let [pos, spd] = q.shift();
-			if (pos === target) return level;
-			let nxt = [pos + spd, spd * 2];
-			let key = nxt.join('#');
-			if (!visited.has(key) && 0 < nxt[0] && nxt[0] < target * 2) {
-				q.push(nxt);
+		for (let k = q.length; k > 0; k--) {
+			let [instr, [pos, spd]] = q.shift();
+			if (pos === target) {
+                console.log(instr);
+                return level;
+            };
+			let nxtState = [pos + spd, spd * 2]
+			let key = nxtState.join('#');
+			if (!visited.has(key) && 0 < nxtState[0] && nxtState[0] < target * 2) {
+				q.push([instr.concat('A'), nxtState]);
 				visited.add(key);
 			}
-			nxt = [pos, spd > 0 ? -1 : 1];
-			key = nxt.join('#');
-			if (!visited.has(key) && 0 < nxt[0] && nxt[0] < target * 2) {
-				q.push(nxt);
+			nxtState = [pos, spd > 0 ? -1 : 1];
+			key = nxtState.join('#');
+			if (!visited.has(key) && 0 < nxtState[0] && nxtState[0] < target * 2) {
+				q.push([instr.concat('R'), nxtState]);
 				visited.add(key);
 			}
 		}
