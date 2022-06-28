d13f69b0b6105846e62d1024e57d18662d96e7bd
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-06-28 18:23:23 -0500

    Time: 75 ms (69.42%), Space: 42.3 MB (50.42%) - LeetHub

--- 366-find-leaves-of-binary-tree/366-find-leaves-of-binary-tree.js
+++ 366-find-leaves-of-binary-tree/366-find-leaves-of-binary-tree.js
@@ -11,21 +11,29 @@
  * @return {number[][]}
  */
 var findLeaves = function(root) {
+    const getHeight = (node) => {
+        if (!node) return 0;
+        return 1 + Math.max(getHeight(node.left), getHeight(node.right));
+    }
+    let n = getHeight(root);
     // travel down the tree
     // when you reach the end, return [height, val]
-    const result = [];
     let map = {};
+    
+    const result = [];
+    for (let i = 0; i < n; i++) result.push([]);
+    
     const dfs = (node) => {
         if (!node) return -1;
         
         let ans = 1 + Math.max(dfs(node.right),dfs(node.left));
-        result.push([ans, node.val]);
+        result[ans].push(node.val);
         return ans;
     }
     dfs(root);
-    result.sort((a,b) => a[0] - b[0]);
-    for (let node of result) {
-        map[node[0]] ? map[node[0]].push(node[1]) : map[node[0]] = [node[1]];
-    }
-    return Object.values(map);
+    // result.sort((a,b) => a[0] - b[0]);
+    // for (let node of result) {
+        // map[node[0]] ? map[node[0]].push(node[1]) : map[node[0]] = [node[1]];
+    // }
+    return result;
 };
