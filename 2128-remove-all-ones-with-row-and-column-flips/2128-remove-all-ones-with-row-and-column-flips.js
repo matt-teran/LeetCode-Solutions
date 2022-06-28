f8b2eebd7f973e03c51241daba686e1c83b02101
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-06-28 18:47:13 -0500

    Time: 90 ms (69.21%), Space: 46.8 MB (11.59%) - LeetHub

--- 2128-remove-all-ones-with-row-and-column-flips/2128-remove-all-ones-with-row-and-column-flips.js
+++ 2128-remove-all-ones-with-row-and-column-flips/2128-remove-all-ones-with-row-and-column-flips.js
@@ -3,16 +3,13 @@
  * @return {boolean}
  */
 var removeOnes = function(grid) {
-    // all rows need to be equal 010 === 101
-    const newGrid = [];
-    const normRow = grid[0].join('');
+    const row = grid[0].join('');
     const invRow = grid[0].map(i => i ^ 1).join('');
-    for (let row of grid) newGrid.push(row.join(''));
     
-    
-    for (let row of newGrid) {
-        if (row !== normRow && row !== invRow) return false;
+    for (let r of grid) {
+        let joinedRow = r.join('');
+        if (joinedRow !== row && joinedRow !== invRow) return false;
     }
-
+    
     return true;
 };
