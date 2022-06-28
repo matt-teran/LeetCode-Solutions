d9538b6ad3780e315db2038c52208a438e042689
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-06-28 12:14:40 -0500

    Time: 1446 ms (5.83%), Space: 94.9 MB (16.79%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.js
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.js
@@ -6,30 +6,33 @@
 var shortestPath = function(grid, k) {
     const ROWS = grid.length;
     const COLS = grid[0].length;
-    const target = [ROWS - 1, COLS - 1];
-    if (k >= ROWS + COLS - 2) return ROWS + COLS - 2;
     const DIR = [[1,0],[-1,0],[0,1],[0,-1]];
+    const target = [ROWS-1,COLS-1];
+    const visited = new Set();
+    const q = [];
     
-    let state = [0,0,k];
-    const q = [[0,state]];
-    const seen = new Set();
-    seen.add(state.join('#'))
+    let initialState = [0,0,k];
+    q.push([0, initialState]);
+    visited.add(initialState.join('#'));
     
     while (q.length) {
-        let [steps, st] = q.shift();
-        let [row,col,e] = st;
+        let [steps, [row,col,remaining]] = q.shift();
+        
         if (row === target[0] && col === target[1]) return steps;
-        for (let [dr,dc] of DIR) {
+        
+        for (let [dr, dc] of DIR) {
             let [r,c] = [row+dr,col+dc];
+            
             if (grid?.[r]?.[c] > -1) {
-                let newElim = e - grid[r][c];
-                let newState = [r,c,newElim];
-                if (newElim >= 0 && !seen.has(newState.join('#'))) {
-                    seen.add(newState.join('#'));
+                let newRemaining = remaining - grid[r][c];
+                let newState = [r,c,newRemaining];
+                if (newRemaining >= 0 && !visited.has(newState.join('#'))) {
                     q.push([steps+1, newState]);
+                    visited.add(newState.join('#'));
                 }
             }
         }
     }
+    
     return -1;
 };
