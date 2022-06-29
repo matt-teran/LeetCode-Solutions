d98106782d58aaaeff8f79d2bbb69d048f2c5044
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-06-28 22:04:41 -0500

    Time: 1987 ms (9.32%), Space: 54.9 MB (38.99%) - LeetHub

---
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.js
@@ -0,0 +1,47 @@
+/**
+ * @param {string[]} recipes
+ * @param {string[][]} ingredients
+ * @param {string[]} supplies
+ * @return {string[]}
+ */
+var findAllRecipes = function(recipes, ingredients, supplies) {
+    const n = recipes.length;
+    const supply = new Set(supplies);
+    const seen = new Set();
+    const result = new Set();
+    const adj = {};
+    for (let i = 0; i < n; i++) adj[recipes[i]] = ingredients[i];
+    for (let i = 0; i < n; i++) {
+        for (let ingredient of ingredients[i]) {
+            if (!(ingredient in adj)) adj[ingredient] = [];
+        }
+    }
+    
+    const dfs = (recipe) => {
+        if (seen.has(recipe)) {
+            return false;
+        }
+
+        if (adj[recipe].length === 0) {
+            return supply.has(recipe);
+        }
+
+        seen.add(recipe);
+        for (let i of adj[recipe]) {
+            if (!dfs(i)) return false;
+        }
+
+        seen.delete(recipe);
+
+        return true;
+    }
+    
+    for (let recipe of recipes) {
+        if (dfs(recipe)) {
+            supply.add(recipe);
+            result.add(recipe);
+        };
+    }
+    
+    return [...result];
+};
