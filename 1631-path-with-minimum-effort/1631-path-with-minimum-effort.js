e66de2d0f728c5f9ac1562590bf79d6fd34d7d53
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-06-30 14:03:19 -0500

    Time: 282 ms (83.27%), Space: 58.7 MB (59.76%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.js
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.js
@@ -5,31 +5,46 @@
 var minimumEffortPath = function(heights) {
     const ROWS = heights.length;
     const COLS = heights[0].length;
-    const DIR = [[1,0],[-1,0],[0,1],[0,-1]];
-    
-    const difficultyMatrix = [];
-    for (let i = 0; i < ROWS; i++) difficultyMatrix.push(Array(COLS).fill(Infinity));
-    difficultyMatrix[0][0] = 0;
-    
-    const visited = new Set();
-    
-    const heap = new MinPriorityQueue({compare: (a,b) => a[0] > b[0]});
-    heap.enqueue([0,0,0]);
-    
-    while (heap.size()) {
-        const [difficulty, row, col] = heap.dequeue();
-        visited.add(row+'#'+col);
-        for (const [dr, dc] of DIR) {
-            const [r,c] = [row+dr,col+dc];
-            if (heights?.[r]?.[c] !== undefined && !visited.has(r+'#'+c)) {
-                const currentDifficulty = Math.abs(heights[r][c] - heights[row][col]);
-                const maxDifficulty = Math.max(currentDifficulty, difficultyMatrix[row][col]);
-                if (difficultyMatrix[r][c] > maxDifficulty) {
-                    difficultyMatrix[r][c] = maxDifficulty;
-                    heap.enqueue([maxDifficulty,r,c]);
-                }
+    if (ROWS === 1 && COLS === 1) return 0;
+    const edgeList = [];
+    for (let r = 0; r < ROWS; r++) {
+        for (let c = 0; c < COLS; c++) {
+            if (r > 0) {
+                let difference = Math.abs(heights[r][c] - heights[r-1][c])
+                edgeList.push([difference, r * COLS + c, (r - 1) * COLS + c]);
+            }
+            if (c > 0) {
+                let difference = Math.abs(heights[r][c] - heights[r][c-1]);
+                edgeList.push([difference, r * COLS + c, r * COLS + c - 1]);
             }
         }
     }
-    return difficultyMatrix[ROWS-1][COLS-1];
+    edgeList.sort((a,b)=>a[0]-b[0]);
+    
+    const root = [];
+    for (let i = 0; i < ROWS*COLS; i++) root.push(i);
+    const rank = Array(ROWS*COLS).fill(0);
+    
+    const find = x => x === root[x] ? x : root[x] = find(root[x]);
+    const union = (x, y) => {
+        let rootX = find(x);
+        let rootY = find(y);
+        
+        if (rootX === rootY) return false;
+        
+        if (rank[rootX] > rank[rootY]) {
+            root[rootY] = rootX;
+        } else if (rank[rootX] < rank[rootY]) {
+            root[rootX] = rootY;
+        } else {
+            root[rootY] = rootX;
+            rank[rootX]++;
+        }
+    }
+    
+    for (const [difference, a, b] of edgeList) {
+        union(a,b);
+        if (find(0) === find(ROWS*COLS-1)) return difference;
+    }
+    return -1;
 };
