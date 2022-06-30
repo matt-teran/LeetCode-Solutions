8b354f633bad4e3027d2313a6586184e08f1c31a
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-06-30 13:31:58 -0500

    Time: 506 ms (48.21%), Space: 64.1 MB (35.06%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.js
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.js
@@ -6,34 +6,30 @@ var minimumEffortPath = function(heights) {
     const ROWS = heights.length;
     const COLS = heights[0].length;
     const DIR = [[1,0],[-1,0],[0,1],[0,-1]];
-    const diffMatrix = [];
-    for (let r = 0; r < ROWS; r++) diffMatrix.push(Array(COLS).fill(Infinity));
-    diffMatrix[0][0] = 0;
+    
+    const difficultyMatrix = [];
+    for (let i = 0; i < ROWS; i++) difficultyMatrix.push(Array(COLS).fill(Infinity));
+    difficultyMatrix[0][0] = 0;
+    
     const visited = new Set();
     
-    const minHeap = new MinPriorityQueue({compare: (a,b) => a[0] > b[0]});
-    minHeap.enqueue([0,0,0]);
+    const heap = new MinPriorityQueue({compare: (a,b) => a[0] > b[0]});
+    heap.enqueue([0,0,0]);
     
-    while(minHeap.size()) {
-        const [difference, row, col] = minHeap.dequeue();
-        
+    while (heap.size()) {
+        const [difficulty, row, col] = heap.dequeue();
         visited.add(row+'#'+col);
-        
-        for (const [dr,dc] of DIR) {
-            let [r,c] = [row+dr,col+dc];
-            
-            if (heights?.[r]?.[c] !== undefined &&
-                !visited.has(r+'#'+c)) {
-                
-                let currentDiff = Math.abs(heights[r][c] - heights[row][col]);
-                
-                let maxDiff = Math.max(currentDiff, diffMatrix[row][col]);
-                if (diffMatrix[r][c] > maxDiff) {
-                    diffMatrix[r][c] = maxDiff;
-                    minHeap.enqueue([maxDiff,r,c]);
+        for (const [dr, dc] of DIR) {
+            const [r,c] = [row+dr,col+dc];
+            if (heights?.[r]?.[c] !== undefined && !visited.has(r+'#'+c)) {
+                const currentDifficulty = Math.abs(heights[r][c] - heights[row][col]);
+                const maxDifficulty = Math.max(currentDifficulty, difficultyMatrix[row][col]);
+                if (difficultyMatrix[r][c] > maxDifficulty) {
+                    difficultyMatrix[r][c] = maxDifficulty;
+                    heap.enqueue([maxDifficulty,r,c]);
                 }
             }
         }
     }
-    return diffMatrix[ROWS-1][COLS-1];
+    return difficultyMatrix[ROWS-1][COLS-1];
 };
