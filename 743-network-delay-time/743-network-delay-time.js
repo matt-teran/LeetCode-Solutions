3e4eed95fec17d629ef942531d548cbd00fa7ac5
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-06-30 01:54:36 -0500

    Time: 215 ms (34.29%), Space: 52.9 MB (28.49%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.js
+++ 743-network-delay-time/743-network-delay-time.js
@@ -5,32 +5,26 @@
  * @return {number}
  */
 var networkDelayTime = function(times, n, k) {
-    //visited set
     const visited = new Set();
     
-    // create adjacency list
-    const edges = {};
-    for (let [source, target, time] of times) edges[source] = [];
-    for (let [source, target, time] of times) edges[source].push([target, time]);
+    const adj = {};
+    for (const [source, target, time] of times) adj[source] = [];
+    for (const [source, target, time] of times) adj[source].push([target, time]);
     
-    // min heap
-    const heap = new MinPriorityQueue({compare: (a, b) => a[1] > b[1]});
-    heap.enqueue([k, 0]);
+    const minHeap = new MinPriorityQueue({compare: (a,b) => a[1] > b[1]});
+    minHeap.enqueue([k, 0]);
     let time = 0;
     
-    // bfs
-    while (heap.size()) {
-        let dequeued = heap.dequeue();
-        let [n1, w1] = dequeued;
+    while (minHeap.size()) {
+        const [n1, w1] = minHeap.dequeue();
         
         if (visited.has(n1)) continue;
         visited.add(n1);
-        
         time = Math.max(time, w1);
         
-        if (n1 in edges) {
-            for (let [n2, w2] of edges[n1]) {
-                if (!visited.has(n2)) heap.enqueue([n2, w1 + w2]);
+        if (n1 in adj) {
+            for (const [n2, w2] of adj[n1]) {
+                if (!visited.has(n2)) minHeap.enqueue([n2, w1 + w2]);
             }
         }
     }
