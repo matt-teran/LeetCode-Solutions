48c0b4d9c332111e18c0ebb53cb9ef90426b1a05
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-06-30 18:38:35 -0500

    Time: 72 ms (77.56%), Space: 44.1 MB (5.94%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
@@ -7,26 +7,40 @@ var search = function(nums, target) {
     let l = 0;
     let r = nums.length - 1;
     
-    while (l <= r) {
-        let m = Math.floor((l + r) / 2);
-        
-        if (target === nums[m]) return m;
-        
-        if (nums[l] <= nums[m]) {
-            if (target > nums[m] || target < nums[l]) {
-                l = m + 1;
+    const findRotationIndex = (l, r) => {
+        if (nums[l] < nums[r]) return 0;
+        while (l<=r) {
+            let pivot = Math.floor((r - l) / 2) + l;
+            if (nums[pivot] > nums[pivot + 1]) return pivot + 1;
+            
+            if (nums[pivot] < nums[l]) {
+                r = pivot - 1;
             } else {
-                r = m - 1;
-            }
-        } else {
-            if (target < nums[m] || target > nums[r]) {
-                r = m - 1;
-            } else {
-                l = m + 1;
+                l = pivot + 1;
             }
         }
     }
-    return -1;
+    const binarySearch = (l, r) => {
+        while (l <= r) {
+            let m = Math.floor((r - l)/2) + l;
+            if (target < nums[m]) {
+                r = m - 1;
+            } else if (target > nums[m]) {
+                l = m + 1;
+            } else {
+                return m;
+            }
+        }
+        return -1;
+    }
+    const p = findRotationIndex(l,r);
+    console.log(p);
+    if (nums.length === 1) return nums[0] === target ? 0 : -1;
+    
+    if (nums[p] === target) return p;
+    if (p === 0) return binarySearch(0, nums.length - 1);
+    if (target < nums[0]) return binarySearch(p, nums.length - 1);
+    return binarySearch(0, p);
 };
 // [4,5,6,7,0,1,2]
 // 1 
