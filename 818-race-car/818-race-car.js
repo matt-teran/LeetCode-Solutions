0a49e0a74fb631946bc520d1b7093e8f205e7ffa
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-07-01 14:01:05 -0500

    Time: 3437 ms (6.92%), Space: 73.7 MB (33.57%) - LeetHub

--- 818-race-car/818-race-car.js
+++ 818-race-car/818-race-car.js
@@ -1,32 +1,33 @@
-const racecar = (target) => {
-    // create our initial state, we start at position 0 with speed 1
-	const initialState = [0,1];
-    // initialize our BFS queue with this state
-	const q = [['', initialState]];
-    // initialize visited set w/ initial state
-	const visited = new Set([initialState.join('#')]);
-
-	for (let level = 0; q.length; level++) {
-		for (let k = q.length; k > 0; k--) {
-			let [instr, [pos, spd]] = q.shift();
-			if (pos === target) {
-                console.log(instr);
-                return level;
-            };
-			let nxtState = [pos + spd, spd * 2]
-			let key = nxtState.join('#');
-			if (!visited.has(key) && 0 < nxtState[0] && nxtState[0] < target * 2) {
-				q.push([instr.concat('A'), nxtState]);
-				visited.add(key);
-			}
-			nxtState = [pos, spd > 0 ? -1 : 1];
-			key = nxtState.join('#');
-			if (!visited.has(key) && 0 < nxtState[0] && nxtState[0] < target * 2) {
-				q.push([instr.concat('R'), nxtState]);
-				visited.add(key);
-			}
-		}
-	}
-
-	return -1;
-}
+/**
+ * @param {number} target
+ * @return {number}
+ */
+var racecar = function(target) {
+    const initialState = [0, 1];
+    const visited = new Set();
+    const q = [initialState];
+    visited.add(initialState.join('#'));
+    
+    for (let level = 0; q.length; level++) {
+        for (let i = q.length; i > 0; i--) {
+            const [pos, spd] = q.shift();
+            
+            if (pos === target) return level;
+            
+            let newState = [pos+spd, spd * 2];
+            let key = newState.join('#');
+            if (!visited.has(key) && 0 < newState[0] && newState[0] < target * 2) {
+                q.push(newState);
+                visited.add(key);
+            }
+            
+            newState = [pos, spd > 0 ? -1 : 1];
+            key = newState.join('#');
+            if (!visited.has(key) && 0 < newState[0] && pos < target * 2) {
+                q.push(newState);
+                visited.add(key);
+            }
+        }
+    }
+    return -1;
+};
