4575d5b5473e9af733807d13a8d0ec038acd2988
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-07-01 17:31:51 -0500

    Time: 260 ms (39.15%), Space: 66.3 MB (5.33%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.js
+++ 1048-longest-string-chain/1048-longest-string-chain.js
@@ -3,26 +3,43 @@
  * @return {number}
  */
 var longestStrChain = function(words) {
-    // const dp = Array(words.length);
+    const map = {};
+    words.sort((a,b) => a.length - b.length);
+    for (let i = 0; i < words.length; i++) map[words[i]] = i;
+    console.log(map);
+    const dp = Array(words.length).fill(1);
     
-    const set = new Set(words);
-    const memo = {};
-    
-    const dp = word => {
-        if (!(word in memo)) {
-            let best = 1;
-            for (let i = 0; i < word.length; i++) {
-                const newWord = word.substring(0,i)+word.substring(i+1);
-                if (!set.has(newWord)) continue;
-                best = Math.max(best, 1 + dp(newWord));
-            }
-            memo[word] = best;
+    for (let i = 0; i < words.length; i++) {
+        let best = 1;
+        for (let j = 0; j < words[i].length; j++) {
+            const newWord = words[i].substring(0,j) + words[i].substring(j+1);
+            if (i === 2) console.log(newWord);
+            if (!(newWord in map)) continue;
+            best = Math.max(best, 1 + dp[map[newWord]]);
         }
-        
-        return memo[word];
+        dp[i] = best;
     }
+    return Math.max(...dp);
     
-    return Math.max(...words.map(word => dp(word)));
+    
+//     const set = new Set(words);
+//     const memo = {};
+    
+//     const dp = word => {
+//         if (!(word in memo)) {
+//             let best = 1;
+//             for (let i = 0; i < word.length; i++) {
+//                 const newWord = word.substring(0,i)+word.substring(i+1);
+//                 if (!set.has(newWord)) continue;
+//                 best = Math.max(best, 1 + dp(newWord));
+//             }
+//             memo[word] = best;
+//         }
+        
+//         return memo[word];
+//     }
+    
+//     return Math.max(...words.map(word => dp(word)));
 };
 
 // State variables: word
