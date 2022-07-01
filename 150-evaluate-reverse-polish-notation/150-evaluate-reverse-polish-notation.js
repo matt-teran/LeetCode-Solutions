02e19dcbe159046d313193f71b2014c4035b5482
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-07-01 16:50:17 -0500

    Time: 83 ms (81.62%), Space: 44.8 MB (71.52%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.js
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.js
@@ -7,17 +7,17 @@ var evalRPN = function(tokens) {
         '+': (a,b) => a + b,
         '-': (a,b) => a - b,
         '*': (a,b) => a * b,
-        '/': (a,b) => a/b < 0 ? Math.ceil(a/b) : Math.floor(a/b)
+        '/': (a,b) => a / b > 0 ? Math.floor(a/b) : Math.ceil(a/b)
     }
     const stack = [];
     
-    for (let token of tokens) {
+    for (const token of tokens) {
         if (token in map) {
-            let b = stack.pop();
-            let a = stack.pop();
+            const b = stack.pop();
+            const a = stack.pop();
             stack.push(map[token](a,b));
         } else {
-            stack.push(Number(token))
+            stack.push(Number(token));
         }
     }
     return stack[0];
