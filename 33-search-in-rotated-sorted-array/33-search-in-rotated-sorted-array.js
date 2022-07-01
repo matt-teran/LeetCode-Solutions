bef332109b0f90c69331ed8cb0a3fb0b7d4cd892
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-07-01 01:21:22 -0500

    Time: 131 ms (5.19%), Space: 44 MB (7.28%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
@@ -6,41 +6,43 @@
 var search = function(nums, target) {
     let l = 0;
     let r = nums.length - 1;
-    
-    const findRotationIndex = (l, r) => {
+    const findRotationIndex = (l,r) => {
         if (nums[l] < nums[r]) return 0;
-        while (l<=r) {
-            let pivot = Math.floor((r - l) / 2) + l;
-            if (nums[pivot] > nums[pivot + 1]) return pivot + 1;
-            
-            if (nums[pivot] < nums[l]) {
-                r = pivot - 1;
+        while (l <= r) {
+            let m = Math.floor((r - l) / 2) + l;
+
+            if (nums[m] > nums[m+1]) {
+                return m + 1;
+            } else if (nums[m] < nums[l]) {
+                r = m - 1;
             } else {
-                l = pivot + 1;
+                l = m + 1;
             }
         }
     }
     const binarySearch = (l, r) => {
-        while (l <= r) {
-            let m = Math.floor((r - l)/2) + l;
-            if (target < nums[m]) {
-                r = m - 1;
-            } else if (target > nums[m]) {
+        while (l<=r) {
+            let m = Math.floor((r - l) / 2) + l;
+            if (nums[m] < target) {
                 l = m + 1;
+            } else if (nums[m] > target) {
+                r = m - 1;
             } else {
                 return m;
             }
         }
         return -1;
     }
-    const p = findRotationIndex(l,r);
-    console.log(p);
     if (nums.length === 1) return nums[0] === target ? 0 : -1;
-    
+    let p = findRotationIndex(l,r);
+    console.log(p);
+    l = 0;
+    r = nums.length - 1;
+    if (p === 0) return binarySearch(l, r);
     if (nums[p] === target) return p;
-    if (p === 0) return binarySearch(0, nums.length - 1);
-    if (target < nums[0]) return binarySearch(p, nums.length - 1);
-    return binarySearch(0, p);
+    if (nums[0] > target) return binarySearch(p,r);
+    return binarySearch(l, p);
+    
+    
+    return findRotationIndex(0, nums.length - 1)
 };
-// [4,5,6,7,0,1,2]
-// 1 
