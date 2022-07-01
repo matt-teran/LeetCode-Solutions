9af372b56a017945fe569259421e206ac751a79f
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-07-01 17:08:41 -0500

    Time: 205 ms (56.61%), Space: 49 MB (76.73%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.js
+++ 1048-longest-string-chain/1048-longest-string-chain.js
@@ -3,20 +3,18 @@
  * @return {number}
  */
 var longestStrChain = function(words) {
-    const wordsPresent = new Set(words);
+    const set = new Set(words);
     const memo = {};
-    const dp = (word) => {
+    
+    const dp = word => {
+        if (!set.has(word)) return 0;
+        
         if (!(word in memo)) {
-            let maxLength = 1;
+            let best = 1;
             for (let i = 0; i < word.length; i++) {
-                const newWord = word.substring(0,i) + word.substring(i+1);
-                if (wordsPresent.has(newWord)) {
-                    let currentLength = 1 + dp(newWord);
-                    maxLength = Math.max(maxLength, currentLength);
-                }
-                
+                best = Math.max(best, 1 + dp(word.substring(0,i)+word.substring(i+1)));
             }
-            memo[word] = maxLength;
+            memo[word] = best;
         }
         
         return memo[word];
