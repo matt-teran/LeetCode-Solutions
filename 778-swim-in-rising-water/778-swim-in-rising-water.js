0ff01b3f9a77ae74790ccf0434503c1adb7b93bc
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-06-30 23:34:39 -0500

    Time: 207 ms (51.11%), Space: 51 MB (51.10%) - LeetHub

--- 778-swim-in-rising-water/778-swim-in-rising-water.js
+++ 778-swim-in-rising-water/778-swim-in-rising-water.js
@@ -4,7 +4,7 @@
  */
 var swimInWater = function(grid) {
     const n = grid.length;
-    if (n === 1) return 0
+    // if (n === 1) return 0
     const DIR = [[1,0],[-1,0],[0,1],[0,-1]];
     const visited = new Set();
     const heap = new MinPriorityQueue({compare: (a,b) => a[0] > b[0]});
@@ -13,7 +13,7 @@ var swimInWater = function(grid) {
     
     while (heap.size()) {
         const [elevation, row, col] = heap.dequeue();
-        
+        if (row === n - 1 && col === n - 1) return elevation;
         visited.add(row+'#'+col);
         
         for (let [dr,dc] of DIR) {
@@ -22,7 +22,6 @@ var swimInWater = function(grid) {
             if (grid?.[r]?.[c] !== undefined &&
                 !visited.has(r+'#'+c)) {
                 let newElevation = Math.max(elevation, grid[r][c]);
-                if (r === n - 1 && c=== n - 1) return newElevation;
                 grid[r][c] = newElevation;
                 heap.enqueue([newElevation, r, c]);
             }
