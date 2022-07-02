99f849d0bb5f0103c57bd17aa675e369d5143aa0
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-07-02 14:50:32 -0500

    Time: 132 ms (77.38%), Space: 48.5 MB (84.01%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.js
+++ 56-merge-intervals/56-merge-intervals.js
@@ -4,14 +4,18 @@
  */
 var merge = function(intervals) {
     intervals.sort((a,b) => a[0] - b[0]);
-    const result = [];
-    for (let i = 0; i < intervals.length - 1; i++) {
-        if (intervals[i][1] < intervals[i+1][0]) {
-            result.push(intervals[i]);
+    const result = [intervals[0]];
+
+    for (let i = 1; i < intervals.length; i++) {
+        const lastEnd = result.at(-1)[1];
+
+        if (intervals[i][0] <= lastEnd) {
+            result.at(-1)[1] = Math.max(lastEnd, intervals[i][1]);
         } else {
-            intervals[i+1] = [Math.min(intervals[i][0], intervals[i+1][0]), Math.max(intervals[i][1], intervals[i+1][1])]
+            result.push(intervals[i]);
         }
+        
     }
-    result.push(intervals.at(-1));
-    return result;
+
+    return result
 };
