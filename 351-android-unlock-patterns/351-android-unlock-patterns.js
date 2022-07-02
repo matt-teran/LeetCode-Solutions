e1c71a4c99674faf0d82ae2a13428d4d9d32d2d9
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-07-02 02:07:58 -0500

    Time: 80 ms (98.78%), Space: 41.8 MB (98.78%) - LeetHub

--- 351-android-unlock-patterns/351-android-unlock-patterns.js
+++ 351-android-unlock-patterns/351-android-unlock-patterns.js
@@ -3,5 +3,12 @@
  * @param {number} n
  * @return {number}
  */
-
-var numberOfPatterns = (m, n) =>  [9,56,320,1624,7152,26016,72912,140704,140704].reduce((acc, num, i) => (m <= i+1 && i+1 <= n) ? acc + num : acc, 0);
+var numberOfPatterns = function(m, n) {
+    let hmm = [0,9,56,320,1624,7152,26016,72912,140704,140704];
+    let sum = 0;
+    while (m <= n) {
+        sum += hmm[m];
+        m++;
+    }
+    return sum;
+};
