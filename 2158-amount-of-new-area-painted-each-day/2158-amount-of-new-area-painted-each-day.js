9073574dda36a7b6aefa87904994b3fb9017b7e1
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-07-02 16:36:58 -0500

    Time: 341 ms (57.49%), Space: 77.5 MB (82.63%) - LeetHub

--- 2158-amount-of-new-area-painted-each-day/2158-amount-of-new-area-painted-each-day.js
+++ 2158-amount-of-new-area-painted-each-day/2158-amount-of-new-area-painted-each-day.js
@@ -3,17 +3,18 @@
  * @return {number[]}
  */
 var amountPainted = function(paint) {
-    let line = Array(50001).fill(0);
-    let result = Array(paint.length).fill(0);
-
+    const line = Array(5 * 10 ** 4).fill(0);
+    const result = Array(paint.length).fill(0);
+    
     for (let i = 0; i < paint.length; i++) {
         let [start, end] = paint[i];
-        while (start < end) {
-            let jump = Math.max(start + 1, line[start]);
+        while(start < end) {
+            const jump = Math.max(line[start], start + 1);
             if (line[start] === 0) result[i]++;
-            line[start] = Math.max(line[start], end);
+            line[start] = end;
             start = jump;
         }
     }
+    
     return result;
 };
