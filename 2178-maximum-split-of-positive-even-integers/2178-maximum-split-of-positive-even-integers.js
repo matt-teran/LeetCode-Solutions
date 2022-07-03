480d1f6273a7b1e1597402f849ce2f681969697e
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-07-03 14:22:44 -0500

    Time: 449 ms (46.71%), Space: 84.2 MB (55.39%) - LeetHub

--- 2178-maximum-split-of-positive-even-integers/2178-maximum-split-of-positive-even-integers.js
+++ 2178-maximum-split-of-positive-even-integers/2178-maximum-split-of-positive-even-integers.js
@@ -4,25 +4,20 @@
  */
 var maximumEvenSplit = function(finalSum) {
     if (finalSum % 2) return [];
-
-    let current = [];
+    const result = [];
     let i = 2;
-    let sumRemaining = finalSum;
-
-    // while using the next number will not overshoot the target sum
-    while (sumRemaining - i >= 0) {
-        current.push(i);
-        sumRemaining -= i;
+    
+    while (finalSum >= i) {
+        finalSum -= i;
+        result.push(i);
         i += 2;
     }
-    // if we hit it exactly, return
-    if (sumRemaining === 0) return current;
-
-    // next number to be added needs to be modified
-    if (sumRemaining <= current.at(-1)) { // last num should be modified otherwise we will have
-        current[current.length - 1] = current.at(-1) + sumRemaining;
-    } else {
-        current.push(sumRemaining);
-    }
-    return current;
+    
+    if (finalSum === 0) return result;
+    
+    if (finalSum - i < 0) {
+        result[result.length - 1] = result.at(-1) + finalSum;
+    } 
+    
+    return result;
 };
