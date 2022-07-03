a009bb830b3f967af98f3b440efa819a68f4eb9f
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-07-02 20:23:05 -0500

    Time: 884 ms (18.75%), Space: 52.6 MB (58.75%) - LeetHub

--- 2013-detect-squares/2013-detect-squares.js
+++ 2013-detect-squares/2013-detect-squares.js
@@ -1,31 +1,30 @@
 
 var DetectSquares = function() {
-    this.points = {}; // r#c: freq
+    this.points = {};
 };
 
 /** 
  * @param {number[]} point
  * @return {void}
  */
-DetectSquares.prototype.add = function(point) {
-    const [r, c] = point;
-    this.points[r+'#'+c] ? this.points[r+'#'+c]++ : this.points[r+'#'+c] = 1;
+DetectSquares.prototype.add = function([x,y]) {
+    this.points[x+'#'+y] ? this.points[x+'#'+y]++ : this.points[x+'#'+y] = 1;
 };
 
 /** 
  * @param {number[]} point
  * @return {number}
  */
-DetectSquares.prototype.count = function(point) {
-    const [x1, y1] = point;
+DetectSquares.prototype.count = function([x1,y1]) {
     let count = 0;
     for (const point in this.points) {
-        let [x3, y3] = point.split('#');
-        if (Math.abs(x3-x1) === 0 || Math.abs(x3 - x1) !== Math.abs(y3 - y1)) continue;
-        count += this.points[point] *
-            (this.points?.[x1 + '#' + y3] || 0) *
-            (this.points?.[x3 + '#' + y1] || 0);
+        const [x3,y3] = point.split('#');
+        
+        if (Math.abs(x3-x1) === 0 || Math.abs(x3-x1) !== Math.abs(y3-y1)) continue;
+        
+        count += this.points[point] * (this.points[x3+'#'+y1] || 0) * (this.points[x1+'#'+y3] || 0);
     }
+    
     return count;
 };
 
