5e3163c2995721c9a33f9bef899cec397d8b6110
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-07-02 19:37:02 -0500

    Time: 67 ms (93.13%), Space: 42.4 MB (72.41%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.js
+++ 191-number-of-1-bits/191-number-of-1-bits.js
@@ -4,6 +4,10 @@
  */
 var hammingWeight = function(n) {
     let res = 0;
-    for (const char of n.toString(2)) char === '1' && res++;
+    let mask = 1;
+    for (let i = 0; i < 32; i++) {
+        if ((n & mask) !== 0) res++;
+        mask <<= 1;
+    }
     return res;
 };
