7d0fec0b186334d5e62f1d05c421f1e1d2bc8b6c
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-07-03 17:04:26 -0500

    Time: 1098 ms (43.24%), Space: 93.6 MB (50.00%) - LeetHub

--- 2135-count-words-obtained-after-adding-a-letter/2135-count-words-obtained-after-adding-a-letter.js
+++ 2135-count-words-obtained-after-adding-a-letter/2135-count-words-obtained-after-adding-a-letter.js
@@ -4,20 +4,18 @@
  * @return {number}
  */
 var wordCount = function(startWords, targetWords) {
-    const startSet = new Set(startWords.map(word => word.split('').sort().join('')));
-    targetWords = targetWords.map(word => word.split('').sort().join(''));
+    const startSet = new Set();
+    for (let word of startWords) startSet.add(word.split('').sort().join('')); // n 
     
     let result = 0;
-    for (const word of targetWords) {
-        
+    for (let word of targetWords) { // n
+        word = word.split('').sort().join(''); 
         for (let i = 0; i < word.length; i++) {
-            let newWord = word.slice(0,i) + word.slice(i+1);
-            if (startSet.has(newWord)) {
-                result++;
-                break;
-            }
+            if (!startSet.has(word.slice(0,i) + word.slice(i+1))) continue;
+            result++;
+            break;
         }
     }
-        
+
     return result;
 };
