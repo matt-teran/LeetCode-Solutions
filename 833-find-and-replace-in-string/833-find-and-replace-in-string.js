8a7b9d49e1583d7e0e59581f79ab5f039804db28
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-07-03 22:47:41 -0500

    Time: 80 ms (63.60%), Space: 43.3 MB (69.96%) - LeetHub

--- 833-find-and-replace-in-string/833-find-and-replace-in-string.js
+++ 833-find-and-replace-in-string/833-find-and-replace-in-string.js
@@ -6,29 +6,16 @@
  * @return {string}
  */
 var findReplaceString = function(s, indices, sources, targets) {
-    let k = indices.length;
-    
-    // Split the string input into an array
-    let constructed = s.split('');
-    
-    // Zip indices, sources, and targets together into conversions
-    let conversions = indices.map((index, i) => [index, sources[i], targets[i]]);
-
-    // Iterate conversions
-    for (const [index, source, target] of conversions) {
-        // If arrayed string doesn't start with source at index, continue
+    const k = indices.length;
+    const zipped = indices.map((index, i) => [index, sources[i], targets[i]]);
+    const constructed = s.split('');
+    for (const [index, source, target] of zipped) {
         if (!s.startsWith(source, index)) continue;
         
-        // Set arrayed string at index to target
         constructed[index] = target;
         
-        // Set the length of 
         constructed.splice(index+1, source.length - 1, ...Array(source.length - 1).fill(''));
-        console.log(constructed);
-        // for (let j = index + 1; j < source.length + index; j++) {
-        //     constructed[j] = '';
-        // }
     }
-
+    
     return constructed.join('');
 };
