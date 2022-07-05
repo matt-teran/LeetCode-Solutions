e68aa4e13fb5b3559b4c749ced14a362d917e2c4
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-07-05 13:52:13 -0500

    Time: 1793 ms (5.40%), Space: 54.4 MB (58.54%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.js
+++ 238-product-of-array-except-self/238-product-of-array-except-self.js
@@ -2,32 +2,21 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-// inputs: array of integers
-// output: array of integers
-// constraints: array length > 1 | time complexity is O(n) | no division operator
-// edge cases: na
 var productExceptSelf = function(nums) {
-    let result = [];
-    // generate prefix and suffix arrays
     let prefix = [];
     let suffix = [];
+    
     for (let i = 0; i < nums.length; i++) {
-        // prefix.push((nums[i - 1] || 0) * prefix[i - 1] || 1);
-        // suffix.unshift((nums[nums.length - i] || 0) * suffix[0] || 1);
-        if (prefix.length === 0) {
-            prefix.push(nums[i]);
-            suffix.push(nums[nums.length - 1]);
-        } else {
-            prefix.push(nums[i] * prefix[i - 1])
-            suffix.unshift(nums[nums.length - 1 - i] * suffix[0]);
-        }
+        let product = (prefix[i-1] ?? 1) * (nums[i-1] ?? 1); // 0, 1, 3, 6, 
+        prefix.push(product);
     }
-    // create a new array of the sums of prefix[i] & suffx[i]
-    //return array
-    console.log(prefix, suffix)
-    return nums.map((num, i) => {
-        return (prefix[i - 1] !== undefined ? prefix[i - 1] : 1) * (suffix[i + 1] !== undefined ? suffix[i + 1] : 1)
-    })
+    for (let i = nums.length - 1; i > -1; i--) {
+        let product = (suffix[0] ?? 1) * (nums[i + 1] ?? 1);
+        suffix.unshift(product);
+    }
+    return prefix.map((pre, i) => pre * suffix[i]);
 };
-// [0, 1, 2, 6]
-// [24, 12, 4, 0]
+
+// 1, 2, 3, 4
+// 1, 1, 2, 6
+// 24, 12, 4, 1
