d3fd36bbf7ed6028f65167592ba9d30a40185bc8
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-07-04 22:01:30 -0500

    Time: 92 ms (35.96%), Space: 41.9 MB (68.61%) - LeetHub

--- 198-house-robber/198-house-robber.js
+++ 198-house-robber/198-house-robber.js
@@ -3,16 +3,23 @@
  * @return {number}
  */
 var rob = function(nums) {
-    const memo = {};
-    const dp = (i) => {
-        if (i > nums.length - 1) return 0;
+    const dp = Array(nums.length + 2).fill(0);
+    // [ 0, 0, 9, 9, 10, 0, 0 ]
+    for (let i = nums.length - 1; i > -1; i--) {
+        dp[i] = Math.max(nums[i] + dp[i + 2], dp[i + 1]);
+    }
+    return dp[0];
+    // console.log(dp);
+//     const memo = {};
+//     const dp = (i) => {
+//         if (i > nums.length - 1) return 0;
         
-        if (!(i in memo)) {
-            memo[i] = Math.max(nums[i] + dp(i+2), dp(i+1));
-        }
+//         if (!(i in memo)) {
+//             memo[i] = Math.max(nums[i] + dp(i+2), dp(i+1));
+//         }
         
-        return memo[i];
-    };
+//         return memo[i];
+//     };
     
-    return dp(0); 
+//     return dp(0); 
 };
