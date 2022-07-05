37d3a2eb7c00344adc3677e673c92a77326c4b9a
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-07-04 23:18:53 -0500

    Time: 110 ms (82.34%), Space: 48.5 MB (41.08%) - LeetHub

--- 221-maximal-square/221-maximal-square.js
+++ 221-maximal-square/221-maximal-square.js
@@ -5,22 +5,17 @@
 var maximalSquare = function(matrix) {
     const R = matrix.length;
     const C = matrix[0].length;
-    const memo = {};
-    
-    const dp = (r, c) => {
-        if (r === R || c === C) return 0;
-        if (matrix[r][c] === '0') return 0;
-        
-        if (!(r in memo)) memo[r] = {};
-        if (!(c in memo[r])) {
-            memo[r][c] = 1 + Math.min(dp(r+1,c), dp(r,c+1), dp(r+1,c+1));
-        }
-        return memo[r][c];
-    };
+    const dp = [];
+    for (let r = 0; r <= R; r++) {
+        dp.push(Array(C+1).fill(0));
+    }
     let result = 0;
-    for (let r = 0; r < R; r++) {
-        for (let c = 0; c < C; c++) {
-            result = Math.max(result, dp(r,c));
+    for (let r = R - 1; r > -1; r--) {
+        for (let c = C - 1; c > -1; c--) {
+            if (matrix[r][c] === '1') {
+                dp[r][c] = 1 + Math.min(dp[r+1][c], dp[r][c+1], dp[r+1][c+1]);
+                result = Math.max(result, dp[r][c]);
+            }
         }
     }
     return result ** 2;
