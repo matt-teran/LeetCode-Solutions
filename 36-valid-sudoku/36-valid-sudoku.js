aaf4449277b7ddd4e2c4830f1ec33727025cb617
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-07-05 14:24:08 -0500

    Time: 161 ms (14.97%), Space: 44.9 MB (75.72%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.js
+++ 36-valid-sudoku/36-valid-sudoku.js
@@ -2,56 +2,57 @@
  * @param {character[][]} board
  * @return {boolean}
  */
-
-// inputs: char[][]
-// output: boolean
-// constraints: input is a sudoku board | each char is 1-9 or .
-// edge cases: empty board
-
 var isValidSudoku = function(board) {
-    // keep track of result
-    // check each 3x3 box for dups
-    let hash = {};
-    for (let i = 0; i < board.length; i++) {
-        for (let j = 0; j < board.length; j++) {
-            if (board[i][j] === '.') continue;
-            let r = Math.floor(i / 3);
-            let c = Math.floor(j / 3);
-            if (JSON.stringify([r, c]) in hash) {
-                if (hash[JSON.stringify([r, c])].has(board[i][j])) {
-                    return false;
-                } else {
-                    hash[JSON.stringify([r, c])].add(board[i][j]);
-                }
-            } else {
-                hash[JSON.stringify([r, c])] = new Set(board[i][j])
-            }
-        }
+    const map = {
+            0: [0,0],
+            1: [0,3],
+            2: [0,6],
+            3: [3,0],
+            4: [3,3],
+            5: [3,6],
+            6: [6,0],
+            7: [6,3],
+            8: [6,6]
     }
-    console.log(hash);
-    // check each row for dups
-    for (let row of board) {
-        let hashset = new Set();
-        for (let char of row) {
-            if (char === '.') continue;
-            if (hashset.has(char)) {
-                return false;
-            } else {
-                hashset.add(char);
-            }
+    const R = board.length;
+    const C = board[0].length;
+    // Checks all columns in row i for dups
+    const checkRow = (r) => {
+        let row = board[r];
+        const seen = new Set();
+        for (const cell of row) {
+            if (cell !== '.' && seen.has(cell)) return false;
+            seen.add(cell);
         }
+        return true;
     }
-    // check each col for dups
-    for (let i = 0; i < board.length; i++) {
-        let hashset = new Set();
-        for (let j = 0; j < board.length; j++) {
-            if (board[j][i] === '.') continue;
-            if (hashset.has(board[j][i])) {
-                return false;
-            } else {
-                hashset.add(board[j][i]);
+    // Checks all rows in column i for dups
+    const checkCol = (c) => {
+        const seen = new Set();
+        for (let r = 0; r < 9; r++) {
+            if (board[r][c] !== '.' && seen.has(board[r][c])) return false;
+            seen.add(board[r][c]);
+        }
+        return true;
+    }
+    // Checks all cells in square i for dups
+    const checkSq = (i) => {
+        const [ROW, COL] = map[i];
+        const seen = new Set();
+        for (let r = ROW; r < ROW + 3; r++) {
+            for (let c = COL; c < COL + 3; c++) {
+                if (board[r][c] !== '.' && seen.has(board[r][c])) return false;
+                seen.add(board[r][c]);
             }
         }
+        return true;
+    }
+    
+    for (let i = 0; i < 9; i++) {
+        if (!checkRow(i) || !checkCol(i) || !checkSq(i)) return false;
     }
     return true;
 };
+// 0,0 - 0,3 - 0,6
+// 3,0 - 3,3 - 3,6
+// 6,0 - 6,3 - 6,6
