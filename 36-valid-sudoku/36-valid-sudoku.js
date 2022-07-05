f19b1b467e5ba07153166bbeb7e5290ea6b0f6b4
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-07-05 16:25:30 -0500

    Time: 88 ms (84.34%), Space: 45.3 MB (58.58%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.js
+++ 36-valid-sudoku/36-valid-sudoku.js
@@ -3,33 +3,24 @@
  * @return {boolean}
  */
 var isValidSudoku = function(board) {
-    const rows = [];
-    const cols = [];
-    const sqrs = [];
+    const N = board.length;
+    const rows = Array(N).fill(0); // 0 => 0 |= (1 << 5)
+    const cols = Array(N).fill(0);
+    const sqrs = Array(N).fill(0);
     
-    for (let i = 0; i < 9; i++) {
-        rows.push(Array(9).fill(0));
-        cols.push(Array(9).fill(0));
-        sqrs.push(Array(9).fill(0));
-    }
-    
-    for (let r = 0; r < 9; r++) {
-        for (let c = 0; c < 9; c++) {
+    for (let r = 0; r < N; r++) {
+        for (let c = 0; c < N; c++) {
             const val = board[r][c];
             const i = Math.floor(r / 3) * 3 + Math.floor(c / 3);
             if (val === '.') continue;
             
-            if (rows[r][val - 1] === 1) return false;
-            if (cols[c][val - 1] === 1) return false;
-            if (sqrs[i][val - 1] === 1) return false;
-            
-            rows[r][val - 1] = 1;
-            cols[c][val - 1] = 1;
-            sqrs[i][val - 1] = 1;
+            if (rows[r] & (1 << val) ||
+                cols[c] & (1 << val) ||
+                sqrs[i] & (1 << val)) return false;
+            rows[r] |= 1 << val;
+            cols[c] |= 1 << val;
+            sqrs[i] |= 1 << val;
         }
     }
     return true;
 };
-// 0,0 - 0,3 - 0,6
-// 3,0 - 3,3 - 3,6
-// 6,0 - 6,3 - 6,6
