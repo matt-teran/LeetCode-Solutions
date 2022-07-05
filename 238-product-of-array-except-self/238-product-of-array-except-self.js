2b08f5cef0b1a9feb5567bffc1694f856be4ca3a
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-07-05 13:57:36 -0500

    Time: 145 ms (59.09%), Space: 58.2 MB (5.71%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.js
+++ 238-product-of-array-except-self/238-product-of-array-except-self.js
@@ -3,20 +3,18 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-    let prefix = [];
-    let suffix = [];
+    const N = nums.length;
+    let prefix = Array(N).fill(0);
+    let suffix = Array(N).fill(0);
     
-    for (let i = 0; i < nums.length; i++) {
-        let product = (prefix[i-1] ?? 1) * (nums[i-1] ?? 1); // 0, 1, 3, 6, 
-        prefix.push(product);
+    for (let i = 0; i < N; i++) {
+        prefix[i] = (prefix[i - 1] ?? 1) * (nums[i - 1] ?? 1); 
+        suffix[N - 1 - i] = (suffix[N - 1 - i + 1] ?? 1) * (nums[N - 1 - i + 1] ?? 1);
     }
-    for (let i = nums.length - 1; i > -1; i--) {
-        let product = (suffix[0] ?? 1) * (nums[i + 1] ?? 1);
-        suffix.unshift(product);
-    }
+    
     return prefix.map((pre, i) => pre * suffix[i]);
 };
 
 // 1, 2, 3, 4
 // 1, 1, 2, 6
-// 24, 12, 4, 1
+// 1, 1, 1, 1
