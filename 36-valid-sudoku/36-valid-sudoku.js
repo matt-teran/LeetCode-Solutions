3a760f27188a2caf33b1d6431e7195a628b40122
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-07-05 15:33:01 -0500

    Time: 81 ms (91.73%), Space: 44.7 MB (84.84%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.js
+++ 36-valid-sudoku/36-valid-sudoku.js
@@ -3,53 +3,31 @@
  * @return {boolean}
  */
 var isValidSudoku = function(board) {
-    const map = {
-            0: [0,0],
-            1: [0,3],
-            2: [0,6],
-            3: [3,0],
-            4: [3,3],
-            5: [3,6],
-            6: [6,0],
-            7: [6,3],
-            8: [6,6]
-    }
-    const R = board.length;
-    const C = board[0].length;
-    // Checks all columns in row i for dups
-    const checkRow = (r) => {
-        let row = board[r];
-        const seen = new Set();
-        for (const cell of row) {
-            if (cell !== '.' && seen.has(cell)) return false;
-            seen.add(cell);
-        }
-        return true;
-    }
-    // Checks all rows in column i for dups
-    const checkCol = (c) => {
-        const seen = new Set();
-        for (let r = 0; r < 9; r++) {
-            if (board[r][c] !== '.' && seen.has(board[r][c])) return false;
-            seen.add(board[r][c]);
-        }
-        return true;
-    }
-    // Checks all cells in square i for dups
-    const checkSq = (i) => {
-        const [ROW, COL] = map[i];
-        const seen = new Set();
-        for (let r = ROW; r < ROW + 3; r++) {
-            for (let c = COL; c < COL + 3; c++) {
-                if (board[r][c] !== '.' && seen.has(board[r][c])) return false;
-                seen.add(board[r][c]);
-            }
-        }
-        return true;
-    }
+    const rows = [];
+    const cols = [];
+    const sqrs = [];
     
     for (let i = 0; i < 9; i++) {
-        if (!checkRow(i) || !checkCol(i) || !checkSq(i)) return false;
+        rows.push(new Set());
+        cols.push(new Set());
+        sqrs.push(new Set());
+    }
+    
+    for (let r = 0; r < 9; r++) {
+        for (let c = 0; c < 9; c++) {
+            const val = board[r][c];
+            const i = Math.floor(r / 3) * 3 + Math.floor(c / 3);
+            
+            if (val === '.') continue;
+            
+            if (rows[r].has(val) ||
+                cols[c].has(val) ||
+                sqrs[i].has(val)) return false;
+            
+            rows[r].add(val);
+            cols[c].add(val);
+            sqrs[i].add(val);
+        }
     }
     return true;
 };
