000c64d9e86ba1e00e530510985fa2cc5ecda05f
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-07-06 12:57:03 -0500

    Time: 89 ms (88.33%), Space: 46.8 MB (77.75%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.js
+++ 210-course-schedule-ii/210-course-schedule-ii.js
@@ -4,39 +4,26 @@
  * @return {number[]}
  */
 var findOrder = function(numCourses, prerequisites) {
-    const WHITE = 1;
-    const GRAY = 2;
-    const BLACK = 3;
+    const result = [];
+    const q = [];
     const adj = {};
+    const inDegree = {};
+    for (let i = 0; i < numCourses; i++) {
+        adj[i] = [];
+        inDegree[i] = 0;
+    };
     for (const [dest, src] of prerequisites) {
-        adj[src] ? adj[src].push(dest) : adj[src] = [dest];
-    }
-    const s = [];
-    let isPossible = true;
-    const color = {};
-    for (let i = 0; i < numCourses; i++) {
-        color[i] = WHITE;
-    }
-    const dfs = (node) => {
-        if (!isPossible) return;
-        
-        color[node] = GRAY;
-        
-        if (node in adj) {
-            for (const neighbor of adj[node]) {
-                if (color[neighbor] === WHITE) {
-                    dfs(neighbor);
-                } else if (color[neighbor] === GRAY) {
-                    isPossible = false;
-                }
-            }
-        }
-        color[node] = BLACK;
-        s.push(node);
-    }
+        adj[src].push(dest)
+        inDegree[dest]++;
+    };
     
-    for (let i = 0; i < numCourses; i++) {
-        if (color[i] === WHITE) dfs(i);
+    for (const course in inDegree) if (!inDegree[course]) q.push(course);
+    while (q.length) {
+        let src = q.shift();
+        for (const dest of adj[src]) {
+            if (!(inDegree[dest]-- - 1)) q.push(dest);
+        }
+        result.push(src);
     }
-    return isPossible ? s.reverse() : [];
+    return result.length === numCourses ? result : [];
 };
