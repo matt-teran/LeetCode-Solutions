ce1fd96c187b2db6823e660d0c9cf5e4c36cb47e
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-07-06 10:40:01 -0500

    Time: 470 ms (91.09%), Space: 47 MB (64.26%) - LeetHub

--- 212-word-search-ii/212-word-search-ii.js
+++ 212-word-search-ii/212-word-search-ii.js
@@ -4,64 +4,58 @@
  * @return {string[]}
  */
 var findWords = function(board, words) {
-    const WORD_KEY = '$';
-
-    
-    const trie = {};
-    for (const word of words) {
-        let node = trie;
-        for (const letter of word) {
-            // Retrieve the next node; If not found, create an empty node
-            if (!(letter in node)) {
-                node[letter] = {};
-            }
-            node = node[letter];
-        }
-        // Mark the existence of a word in trie node
-        node[WORD_KEY] = word;
-    }
-    console.log(trie);
     const ROWS = board.length;
     const COLS = board[0].length;
     const DIR = [[1,0],[-1,0],[0,1],[0,-1]];
+    const WORD_KEY = '$';
 
-    const matchedWords = [];
+    //////////////////////////////////// IMPLEMENT TRIE
+    const trie = new Map();
+    for (const word of words) {
+        let node = trie;
+        for (const letter of word) {
+            if (!(node.has(letter))) {
+                node.set(letter, new Map());
+            }
+            node = node.get(letter);
+        }
+        node.set(WORD_KEY, word)
+    }
+    
+    
+    const result = [];
 
     const backtrack = (row, col, parent) => {
         const letter = board[row][col];
-        const currentNode = parent[letter];
+        const currentNode = parent.get(letter); // trie.d
 
-        // Check if we find a match of word
-        if (WORD_KEY in currentNode) {
-            // We remove the matched word to avoid duplicates,
-            //  as well as avoiding using a hashset for results.
-            matchedWords.push(currentNode[WORD_KEY]);
-            delete currentNode[WORD_KEY];
+        if (currentNode.has(WORD_KEY)) {
+            result.push(currentNode.get(WORD_KEY));
+            currentNode.delete(WORD_KEY);
         }
 
-        // Before the exploration, mark the cell as visited
         board[row][col] = '#';
 
-        // Explore the neighbors in 4 directions, i.e. up, right, down, left
         for (const [dr,dc] of DIR) {
-            let [r, c] = [row + dr, col + dc];
-            if (board?.[r]?.[c] === undefined || !(board[r][c] in currentNode)) continue;
+            const [r, c] = [row + dr, col + dc];
+            if (!board?.[r]?.[c] || !currentNode.has(board[r][c])) continue;
             backtrack(r, c, currentNode);
         }
 
-        // End of exploration, we restore the cell
         board[row][col] = letter;
 
         // Optimization: incrementally remove the matched leaf node in Trie
-        if (!currentNode) delete parent[letter]
+        if (!currentNode.size) parent.delete(letter)
     }
+    
+    ////////////////////////////////////////// APPLY BACKTRACKING
     for (let r = 0; r < ROWS; r++) {
         for (let c = 0; c < COLS; c++) {
             // Starting from each of the cells
-            if (board[r][c] in trie) {
+            if (trie.has(board[r][c])) {
                 backtrack(r, c, trie);
             }
         }
     }
-    return matchedWords;
+    return result;
 };
