074b8975cb8e16bb245070aa4480d11def6d3209
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-07-06 13:55:02 -0500

    Time: 320 ms (42.21%), Space: 56.8 MB (12.68%) - LeetHub

--- 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.js
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.js
@@ -4,36 +4,46 @@
  * @param {string[]} supplies
  * @return {string[]}
  */
-var findAllRecipes = function(recipes, ingredients, supplies) {
-    const available = new Set(supplies);
-    const result = [];
-    const ingredientToRecipe = {};
-    const inDegree = {};
-    const zip = (a, b) => a.map((el,i) => [el, b[i]]);
+var zip = (a,b) => a.map((el,i) => [el, b[i]]);
 
-    for (const [rcp, ingredient] of zip(recipes, ingredients)) {
-        let nonAvailable = 0;
-        for (const ing of ingredient) {
-            if (!available.has(ing)) {
-                nonAvailable++;
-                if (!(ing in ingredientToRecipe)) ingredientToRecipe[ing] = new Set();
-                ingredientToRecipe[ing].add(rcp);
-            }
+var findAllRecipes = function(recipes, ingredients, supplies) {
+    const result = [];
+    
+    const supply = new Set(supplies);
+    const adj = {};
+    const indegree = {};
+    for (const item of supplies) {
+        adj[item] = []
+        indegree[item] = 0;
+    };
+    for (const [recipe, ing] of zip(recipes, ingredients)) {
+        if (!adj[recipe]) adj[recipe] = [];
+        indegree[recipe] = 0;
+        
+        for (const i of ing) {
+            
+            adj[i] ? adj[i].push(recipe) : adj[i] = [recipe];
+            
+            indegree[recipe]++;
         }
-        if (nonAvailable === 0) {
-            result.push(rcp);
-        } else {
-            inDegree[rcp] = nonAvailable;
-        }
+        if (recipe === 'izcad') console.log(adj);
+        // if (recipe === 'izcad') console.log(indegree);
     }
-    for (const rcp of result) {
-        if (rcp in ingredientToRecipe) {
-            for (const recipe of ingredientToRecipe[rcp]) {
-                inDegree[recipe]--;
-                if (inDegree[recipe] === 0) result.push(recipe);
-            }
-            delete ingredientToRecipe[rcp];
+    const q = [...supplies];
+    // for (const key in indegree) if (!indegree[key]) q.push(key);
+    // console.log(adj);
+    while (q.length) {
+        const src = q.shift();
+        if (src === 'g') console.log(adj['g'])
+        for (const dest of adj[src]) {
+            
+            indegree[dest]--;
+            if (indegree[dest] === 0) q.push(dest);
+            
         }
+        // result.push(src);
+        if (!supply.has(src)) result.push(src);
     }
+    // console.log(q);
     return result;
 };
