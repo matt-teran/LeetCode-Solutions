8fec5a9435b83daf21a5cf84cf3e0b7bd7753a47
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-07-06 16:38:47 -0500

    Time: 266 ms (23.03%), Space: 56.8 MB (18.88%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.js
+++ 743-network-delay-time/743-network-delay-time.js
@@ -6,27 +6,21 @@
  */
 var networkDelayTime = function(times, n, k) {
     const visited = new Set();
-    
     const adj = {};
-    for (const [source, target, time] of times) adj[source] = [];
-    for (const [source, target, time] of times) adj[source].push([target, time]);
+    for (let i = 1; i <= n; i++) adj[i] = [];
+    for (const [source, target, weight] of times) adj[source].push([target, weight]);
     
-    const minHeap = new MinPriorityQueue({compare: (a,b) => a[1] > b[1]});
-    minHeap.enqueue([k, 0]);
+    const q = new MinPriorityQueue({compare: (a,b) => a[1] > b[1]});
     let time = 0;
+    q.enqueue([k, 0]);
     
-    while (minHeap.size()) {
-        const [n1, w1] = minHeap.dequeue();
-        
-        if (visited.has(n1)) continue;
-        visited.add(n1);
-        time = Math.max(time, w1);
-        
-        if (n1 in adj) {
-            for (const [n2, w2] of adj[n1]) {
-                if (!visited.has(n2)) minHeap.enqueue([n2, w1 + w2]);
-            }
-        }
+    while (q.size()) {
+        let [n1, w1] = q.dequeue();
+        if (!visited.has(n1)) {
+            time = Math.max(time, w1);
+            visited.add(n1);
+            for (const [n2, w2] of adj[n1]) q.enqueue([n2, w1+w2]);
+        };
     }
     
     return visited.size === n ? time : -1;
