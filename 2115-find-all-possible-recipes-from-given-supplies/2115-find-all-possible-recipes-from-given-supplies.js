d62d64ef01ef8f8ae689edf1da9b0b75fb2d4361
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-07-06 14:00:36 -0500

    Time: 199 ms (83.51%), Space: 56.4 MB (13.04%) - LeetHub

--- 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.js
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.js
@@ -12,38 +12,22 @@ var findAllRecipes = function(recipes, ingredients, supplies) {
     const supply = new Set(supplies);
     const adj = {};
     const indegree = {};
-    for (const item of supplies) {
-        adj[item] = []
-        indegree[item] = 0;
-    };
+
     for (const [recipe, ing] of zip(recipes, ingredients)) {
-        if (!adj[recipe]) adj[recipe] = [];
         indegree[recipe] = 0;
         
         for (const i of ing) {
-            
-            adj[i] ? adj[i].push(recipe) : adj[i] = [recipe];
-            
+            adj[i] ? adj[i].push(recipe) : adj[i] = [recipe];   
             indegree[recipe]++;
         }
-        if (recipe === 'izcad') console.log(adj);
-        // if (recipe === 'izcad') console.log(indegree);
     }
+    
     const q = [...supplies];
-    // for (const key in indegree) if (!indegree[key]) q.push(key);
-    // console.log(adj);
     while (q.length) {
         const src = q.shift();
-        if (src === 'g') console.log(adj['g'])
-        for (const dest of adj[src]) {
-            
-            indegree[dest]--;
-            if (indegree[dest] === 0) q.push(dest);
-            
-        }
-        // result.push(src);
+        for (const dest of adj[src] || []) if (!(indegree[dest]-- - 1)) q.push(dest);
         if (!supply.has(src)) result.push(src);
     }
-    // console.log(q);
+
     return result;
 };
