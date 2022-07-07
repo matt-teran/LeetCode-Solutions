2e2ccd3f2aede08acb034ab988679e8b7192e9ee
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-07-06 19:05:03 -0500

    Time: 156 ms (70.50%), Space: 51.3 MB (40.61%) - LeetHub

--- 778-swim-in-rising-water/778-swim-in-rising-water.js
+++ 778-swim-in-rising-water/778-swim-in-rising-water.js
@@ -4,23 +4,21 @@
  */
 var swimInWater = function(grid) {
     const n = grid.length;
-    const visited = new Set();
     const DIR = [[1,0],[-1,0],[0,1],[0,-1]];
     const q = new MinPriorityQueue({compare: (a,b) => a[0] > b[0]});
-    visited.add(0+'#'+0);
     q.enqueue([grid[0][0], [0,0]]);
     let elevation = grid[0][0];
+    grid[0][0] = '#';
     
     while (q.size()) {
         let [depth, [x1, y1]] = q.dequeue();
-        visited.add(x1 + '#' + y1);
+        grid[x1][y1] = '#';
         elevation = Math.max(depth, elevation)
         if (x1 === n - 1 && y1 === n - 1) return elevation;
         
         for (const [dx, dy] of DIR) {
             const [x2, y2] = [x1 + dx, y1 + dy];
-            if (grid?.[x2]?.[y2] !== undefined &&
-                !visited.has(x2 + '#' + y2)) {
+            if (grid?.[x2]?.[y2] > -1) {
                 q.enqueue([grid[x2][y2], [x2, y2]]);
             }
         }
