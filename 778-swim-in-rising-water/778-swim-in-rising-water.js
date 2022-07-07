9a7cc9715ac7a5138c03cac7a97d33a3776bb87d
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-07-06 19:03:18 -0500

    Time: 291 ms (25.29%), Space: 51.3 MB (38.70%) - LeetHub

--- 778-swim-in-rising-water/778-swim-in-rising-water.js
+++ 778-swim-in-rising-water/778-swim-in-rising-water.js
@@ -4,26 +4,24 @@
  */
 var swimInWater = function(grid) {
     const n = grid.length;
-    // if (n === 1) return 0
-    const DIR = [[1,0],[-1,0],[0,1],[0,-1]];
     const visited = new Set();
-    const heap = new MinPriorityQueue({compare: (a,b) => a[0] > b[0]});
-    heap.enqueue([grid[0][0],0,0]);
-    visited.add('0'+'#'+'0');
+    const DIR = [[1,0],[-1,0],[0,1],[0,-1]];
+    const q = new MinPriorityQueue({compare: (a,b) => a[0] > b[0]});
+    visited.add(0+'#'+0);
+    q.enqueue([grid[0][0], [0,0]]);
+    let elevation = grid[0][0];
     
-    while (heap.size()) {
-        const [elevation, row, col] = heap.dequeue();
-        if (row === n - 1 && col === n - 1) return elevation;
-        visited.add(row+'#'+col);
+    while (q.size()) {
+        let [depth, [x1, y1]] = q.dequeue();
+        visited.add(x1 + '#' + y1);
+        elevation = Math.max(depth, elevation)
+        if (x1 === n - 1 && y1 === n - 1) return elevation;
         
-        for (let [dr,dc] of DIR) {
-            const [r,c] = [row+dr,col+dc];
-            
-            if (grid?.[r]?.[c] !== undefined &&
-                !visited.has(r+'#'+c)) {
-                let newElevation = Math.max(elevation, grid[r][c]);
-                grid[r][c] = newElevation;
-                heap.enqueue([newElevation, r, c]);
+        for (const [dx, dy] of DIR) {
+            const [x2, y2] = [x1 + dx, y1 + dy];
+            if (grid?.[x2]?.[y2] !== undefined &&
+                !visited.has(x2 + '#' + y2)) {
+                q.enqueue([grid[x2][y2], [x2, y2]]);
             }
         }
     }
