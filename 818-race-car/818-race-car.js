f9848c71466c4ab2c3f7915c7c5d68eac968db53
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-07-06 20:22:57 -0500

    Time: 120 ms (74.18%), Space: 47.1 MB (62.18%) - LeetHub

--- 818-race-car/818-race-car.js
+++ 818-race-car/818-race-car.js
@@ -3,17 +3,38 @@
  * @return {number}
  */
 const racecar = (target) => {
+    const state = [0, 1] // pos, spd
+    const q = [['', state]];
+    const visited = new Set(state.join('#'));
+    
+    for (let level = 0; q.length; level++) {
+        for (let i = q.length; i > 0; i--) {
+            let [dir, [pos, spd]] = q.shift();
+        
+            if (pos === target) {
+                console.log(dir.split('R'));
+                return level;
+            }
 
-    let q = [[0, 1, 0]];
-
-    while (q.length) {
-        let [pos, spd, n] = q.shift();
-        if (pos === target) return n;
-        q.push([pos+spd, spd*2, n+1]);
-        if (spd > 0) {
-            if (pos + spd > target) q.push([pos, -1, n+1]);
-        } else {
-            if (pos + spd < target) q.push([pos, 1, n+1]);
+            let newState = [pos + spd, spd * 2];
+            if (!visited.has(newState.join('#')) && pos > -1 && pos <= target*2) {
+                q.push([dir+'A', newState]);
+                visited.add(newState.join('#'))
+            }
+            
+            if (spd > 0) {
+                newState = [pos, -1]
+                if (spd + pos > target && !visited.has(newState.join('#'))) {
+                    visited.add(newState.join('#'));
+                    q.push([dir+'R', newState]);
+                }
+            } else {
+                newState = [pos, 1]
+                if (spd + pos < target && !visited.has(newState.join('#'))) {
+                    visited.add(newState.join('#'));
+                    q.push([dir+'R', newState]);
+                }
+            }
         }
     }
 };
