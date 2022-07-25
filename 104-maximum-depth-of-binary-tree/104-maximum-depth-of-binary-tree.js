80fac52cd9be30de68604880b739e3ea04730f4b
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-07-25 10:59:40 -0500

    Time: 96 ms (58.74%), Space: 45 MB (74.67%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.js
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.js
@@ -11,18 +11,7 @@
  * @return {number}
  */
 var maxDepth = function(root) {
-    let stack = [[root, 1]];
-    let result = 0;
+    if (!root) return 0;
     
-    while (stack.length) {
-        let [node, depth] = stack.pop();
-        
-        if (node) {
-            result = Math.max(depth, result);
-            
-            stack.push([node.left, depth + 1])
-            stack.push([node.right, depth + 1])
-        }
-    }
-    return result;
+    return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
 };
