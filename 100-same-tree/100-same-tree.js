1cb2bac728a1e80c07c156cde7868396cefb0528
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-07-25 11:37:56 -0500

    Time: 60 ms (95.06%), Space: 42.4 MB (49.80%) - LeetHub

--- 100-same-tree/100-same-tree.js
+++ 100-same-tree/100-same-tree.js
@@ -12,11 +12,7 @@
  * @return {boolean}
  */
 var isSameTree = function(p, q) {
-    if (p === null && q === null) {
-        return true;
-    } else if (p === null || q === null || p.val !== q.val) {
-        return false;
-    };
-    
+    if (!p && !q) return true;
+    if (p?.val !== q?.val) return false;
     return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 };
