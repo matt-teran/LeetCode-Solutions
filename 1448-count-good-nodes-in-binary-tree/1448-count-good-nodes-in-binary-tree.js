dda93b98c2c6edeec3e710f262423e7143772035
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-07-25 14:07:08 -0500

    Time: 243 ms (21.96%), Space: 70 MB (67.76%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.js
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.js
@@ -11,14 +11,17 @@
  * @return {number}
  */
 var goodNodes = function(root) {
-    let count = 0;
-    const dfs = (node, max) => {
-        if (!node) return node;
-        // console.log(node, max)
-        if (node.val >= max) count++;
-        dfs(node.left,Math.max(node.val,max));
-        dfs(node.right,Math.max(node.val,max));
+    if (!root) return 0;
+    let result = 0;
+    const dfs = (node, highest) => {
+        if (!node) return;
+        if (node.val >= highest) result++;
+        
+        dfs(node.left, Math.max(highest, node.val));
+        dfs(node.right, Math.max(highest, node.val));
     }
+    
     dfs(root, root.val);
-    return count;
+    
+    return result;
 };
