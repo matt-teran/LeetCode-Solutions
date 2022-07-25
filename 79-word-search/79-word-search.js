d8e8080da2eae554eaa199eb5e0805c96d8247e6
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-07-25 17:25:13 -0500

    Time: 1720 ms (26.54%), Space: 48.7 MB (30.53%) - LeetHub

--- 79-word-search/79-word-search.js
+++ 79-word-search/79-word-search.js
@@ -4,24 +4,34 @@
  * @return {boolean}
  */
 var exist = function(board, word) {
-    const ROWS = board.length;
-    const COLS = board[0].length;
-    
-    const dfs = (r, c, i) => {
+    const DIR = [[1, 0], [-1, 0], [0, -1], [0, 1]];
+    const backtrack = (row, col, i) => {
         if (i === word.length) return true;
-        if (r < 0 || r >= ROWS ||
-           c < 0 || c >= COLS ||
-           board[r][c] !== word[i]) return false;
-        board[r][c] = '#';
-        const ans = (dfs(r+1,c,i+1) || dfs(r-1,c,i+1) ||
-           dfs(r,c+1,i+1) || dfs(r,c-1,i+1))
-        board[r][c] = word[i];
-        return ans;
+        const q = [[row, col]];
+        
+        while (q.length) {
+            let [r, c] = q.shift();
+            
+            for (let [dr, dc] of DIR) {
+                let [nr, nc] = [r + dr, c + dc];
+                if (board?.[nr]?.[nc] === word[i]) {
+                    board[nr][nc] = '#';
+                    if (backtrack(nr, nc, i+1)) return true;
+                    board[nr][nc] = word[i];
+                }
+            }
+        }
+        return false;
+        
     }
     
-    for (let r = 0; r < ROWS; r++) {
-        for (let c = 0; c < COLS; c++) {
-            if (board[r][c] === word[0] && dfs(r, c, 0)) return true;
+    for (let r = 0; r < board.length; r++) {
+        for (let c = 0; c < board[r].length; c++) {
+            if (board[r][c] === word[0]) {
+                board[r][c] = '#';
+                if (backtrack(r, c, 1)) return true;
+                board[r][c] = word[0];
+            }
         }
     }
     
