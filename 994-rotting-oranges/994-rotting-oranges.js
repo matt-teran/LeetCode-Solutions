91408fe9fd1c2a4a19af1942fd26c1ed46bd55aa
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-07-26 13:28:52 -0500

    Time: 109 ms (60.46%), Space: 45.5 MB (32.46%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.js
+++ 994-rotting-oranges/994-rotting-oranges.js
@@ -3,47 +3,41 @@
  * @return {number}
  */
 var orangesRotting = function(grid) {
-    let minutes = 0;
-    let rottenOranges = [];
+    const ROWS = grid.length;
+    const COLS = grid[0].length;
+    const DIR = [[1,0],[-1,0],[0,1],[0,-1]];
     
-    let rows = grid.length;
-    let cols = grid[0].length;
+    const q = [];
     
-    const bfs = (firstLayer) => {
-        const q = [...firstLayer];
-        const directions = [[1, 0], [-1, 0], [0, 1],[0, -1]];
-        while (q.length) {
-            minutes++;
-            let qLen = q.length;
-            for (let i = 0; i < qLen; i++) {
-                let [row, col] = q.shift();
-                for (let [dr, dc] of directions) {
-                    let [r, c] = [row + dr, col + dc];
+    for (let r = 0; r < ROWS; r++) {
+        for (let c = 0; c < COLS; c++) {
+            if (grid[r][c] === 2) q.push([r,c]);
+        }
+    }
 
-                    if (0 <= r && r < rows &&
-                       0 <= c && c < cols &&
-                        grid[r][c] === 1) {
-                        grid[r][c] = 2;
-                        q.push([r, c]);
-                    }
-                }    
-            }   
+    let minutes = 0;
+    
+    while (q.length) {
+        let increment = false;
+        for (let i = q.length; i > 0; i--) {
+            const [row, col] = q.shift();
+            for (const [dr, dc] of DIR) {
+                let [r,c] = [row + dr, col + dc];
+                
+                if (grid?.[r]?.[c] === 1) {
+                    increment = true;
+                    grid[r][c] = 2;
+                    q.push([r,c]);
+                }
+            }
         }
+        if (increment) minutes++;
     }
     
-    for (let r = 0; r < rows; r++) {
-        for (let c = 0; c < cols; c++) {
-            if (grid[r][c] === 2) rottenOranges.push([r, c]);
-        }
-    }
-    
-    bfs(rottenOranges);
-    
-    for (let r = 0; r < rows; r++) {
-        for (let c = 0; c < cols; c++) {
+    for (let r = 0; r < ROWS; r++) {
+        for (let c = 0; c < COLS; c++) {
             if (grid[r][c] === 1) return -1;
         }
     }
-    
-    return minutes === 0 ? 0 : minutes - 1;
+    return minutes;
 };
