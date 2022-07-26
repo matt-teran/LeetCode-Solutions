bbb01fd17c4ca08e862023e2929479d68de941bd
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-07-26 13:12:08 -0500

    Time: 159 ms (29.83%), Space: 48.8 MB (30.54%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.js
+++ 130-surrounded-regions/130-surrounded-regions.js
@@ -3,43 +3,46 @@
  * @return {void} Do not return anything, modify board in-place instead.
  */
 var solve = function(board) {
-    let rows = board.length;
-    let cols = board[0].length;
+    const ROWS = board.length;
+    const COLS = board[0].length;
+    const DIR = [[1,0],[-1,0],[0,1],[0,-1]];
     
-    const dfs = (row, col) => {
-        if (row < 0 || row === rows ||
-           col < 0 || col === cols ||
-           board[row][col] !== 'O') return;
-        board[row][col] = 'T';
+    const bfs = (cell) => {
+        const q = [cell];
         
-        dfs(row + 1, col);
-        dfs(row - 1, col);
-        dfs(row, col + 1);
-        dfs(row, col - 1);
-    }
+        while (q.length) {
+            const [row, col] = q.shift();
+            
+            for (const [dr, dc] of DIR) {
+                const [r, c] = [row + dr, col + dc];
+                
+                if (board?.[r]?.[c] === 'O') {
+                    board[r][c] = 'T';
+                    q.push([r, c]);
+                }
+            }
+        }
+    };
     
-    for (let r = 0; r < rows; r++) {
-        for (let c = 0; c < cols; c++) {
-            if (board[r][c] === 'O' &&
-               (r === 0 || r === rows - 1 ||
-               c === 0 || c === cols - 1)) {
-                dfs(r, c);
+    for (let r = 0; r < ROWS; r++) {
+        for (let c = 0; c < COLS; c++) {
+            if (board[r][c] === 'O' && (r === 0 || r === ROWS - 1 ||
+               c === 0 || c === COLS - 1)) {
+                board[r][c] = 'T';
+                bfs([r, c]);
             }
         }
     }
     
-    for (let r = 0; r < rows; r++) {
-        for (let c = 0; c < cols; c++) {
+    for (let r = 0; r < ROWS; r++) {
+        for (let c = 0; c < COLS; c++) {
             if (board[r][c] === 'O') {
-                board[r][c] = 'X'
+                board[r][c] = 'X';
             } else if (board[r][c] === 'T') {
-                board[r][c] = 'O'
+                board[r][c] = 'O';
             }
         }
     }
-    // for (let r = 0; r < rows; r++) {
-    //     for (let c = 0; c < cols; c++) {
-    //         if (board[r][c] === 'T') board[r][c] = 'O';
-    //     }
-    // }
+    
+    return board;
 };
