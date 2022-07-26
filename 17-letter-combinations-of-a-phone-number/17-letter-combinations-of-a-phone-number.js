bd9c768f71f2b86aa8e43f3341b432cc4346e5d0
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-07-26 11:06:33 -0500

    Time: 84 ms (51.74%), Space: 42.3 MB (31.35%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.js
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.js
@@ -3,36 +3,29 @@
  * @return {string[]}
  */
 const map = {
-    2: ['a', 'b', 'c'],
-    3: ['d', 'e', 'f'],
-    4: ['g', 'h', 'i'],
-    5: ['j', 'k', 'l'],
-    6: ['m', 'n', 'o'],
-    7: ['p', 'q', 'r', 's'],
-    8: ['t', 'u', 'v'],
-    9: ['w', 'x', 'y', 'z']
+    2: 'abc',
+    3: 'def',
+    4: 'ghi',
+    5: 'jkl',
+    6: 'mno',
+    7: 'pqrs',
+    8: 'tuv',
+    9: 'wxyz'
 }
 
 var letterCombinations = function(digits) {
-    if (!digits.length) return [];
-  // we have the option to press digits[i] map[digits[i]].length times
+    if (digits.length === 0) return [];
     const result = [];
-    const combo = [];
-    const dfs = (i) => {
-        if (i === digits.length) return combo.length === digits.length && result.push([...combo].join(''));
-        
-        for (let j = i; j < digits.length; j++) {
-            let n = 0;
-            while (n < map[digits[j]].length) {
-                combo.push(map[digits[i]][n]);
-                dfs(j + 1);
-                combo.pop();
-                n++;
-            }
-        }
-    }
     
-    dfs(0);
+    const backtrack = (i, combo) => {
+        if (combo.length === digits.length) return result.push(combo);
+        
+        for (const c of map[digits[i]]) {
+            backtrack(i + 1, combo + c);
+        }
+    };
+    
+    backtrack(0, '');
     
     return result;
 };
