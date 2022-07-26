f0a742daf120ff517ea3f6d083ac7ab62e0ff329
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-07-26 11:55:24 -0500

    Time: 146 ms (29.87%), Space: 47.2 MB (35.75%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.js
+++ 695-max-area-of-island/695-max-area-of-island.js
@@ -3,30 +3,42 @@
  * @return {number}
  */
 var maxAreaOfIsland = function(grid) {
-    if (!grid.length) return 0;
-    let result = 0;
-    let rows = grid.length;
-    let cols = grid[0].length;
-    let visited = new Set();
+    const ROWS = grid.length;
+    const COLS = grid[0].length;
+    const DIR = [[1,0],[-1,0],[0,1],[0,-1]];
     
-    const dfs = (r, c) => {
-        if (r < 0 || r === rows ||
-           c < 0 || c === cols ||
-           grid[r][c] === 0 ||
-           visited.has(`r${r}c${c}`)) {
-            return 0;
+    let maxArea = 0;
+    
+    const bfs = (row, col) => {
+        let A = 0;
+        
+        const q = [[row, col]];
+        
+        while (q.length) {
+            A++;
+            
+            let [r, c] = q.shift();
+            
+            for (const [dr, dc] of DIR) {
+                const [nr, nc] = [r + dr, c + dc];
+                
+                if (grid?.[nr]?.[nc] === 1) {
+                    grid[nr][nc] = '#';
+                    q.push([nr, nc]);
+                }
+            }
         }
-        visited.add(`r${r}c${c}`);
-        return 1 + dfs(r + 1, c) + dfs(r - 1, c) + dfs(r, c + 1) + dfs(r, c - 1)
+        maxArea = Math.max(A, maxArea);
     }
     
-    for (let r = 0; r < rows; r++) {
-        for (let c = 0; c < cols; c++) {
+    for (let r = 0; r < ROWS; r++) {
+        for (let c = 0; c < COLS; c++) {
             if (grid[r][c] === 1) {
-                result = Math.max(result, dfs(r, c));
+                grid[r][c] = '#';
+                bfs(r,c);
             }
         }
     }
     
-    return result;
+    return maxArea;
 };
