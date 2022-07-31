08e1599a944cd17e55646e915bf1f4e710ae6246
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-07-31 15:58:04 -0500

    Time: 144 ms (16.84%), Space: 44.9 MB (62.41%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.js
+++ 684-redundant-connection/684-redundant-connection.js
@@ -3,43 +3,33 @@
  * @return {number[]}
  */
 var findRedundantConnection = function(edges) {
-    const parent = [];
-    for (let i = 0; i <= edges.length; i++) {
-        parent.push(i);
-    }
-    const rank = Array(edges.length + 1).fill(1);
+    const root = [0];
+    const rank = [];
     
-    const find = (n) => {
-        let p = parent[n];
-        while (p !== parent[p]){
-            parent[p] = parent[parent[p]];
-            p = parent[p];
-        }
-        return p;
+    for (let i = 0; i < edges.length; i++) {
+        rank.push(1);
+        root.push(i + 1);
     }
+
+    const find = (x) => root[x] === x ? root[x] : find(root[x]);
     
-    const union = (n1, n2) => {
-        let [p1, p2] = [find(n1), find(n2)];
-        if (p1 === p2) {
-            return false;
-        }
-        if (rank[p1] > rank[p2]) {
-            parent[p2] = p1;
-            rank[p1] += rank[p2];
-        }  else {
-            parent[p1] = p2;
-            rank[p2] += rank[p1];
+    const union = (x, y) => {
+        let rootX = find(x);
+        let rootY = find(y);
+        
+        if (rootX === rootY) return false
+        
+        if (rank[rootX] > rank[rootY]) {
+            root[rootY] = rootX;
+            rank[rootX] += rank[rootY];
+        } else {
+            root[rootX] = rootY;
+            rank[rootY] += rank[rootX];
         }
         return true;
     }
-    for (let [n1, n2] of edges) {
-        if (!union(n1, n2)) return [n1, n2];
+    
+    for (const [x, y] of edges) {
+        if (!union(x, y)) return [x, y];
     }
 };
-
-
-// 1 - 2, 4, 5
-// 2 - 1, 3
-// 3 - 2, 4
-// 4 - 3, 1
-// 5 - 1
