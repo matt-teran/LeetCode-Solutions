029b3c6d35fa63f5ea096ca5928608221c23c24c
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-07-31 16:06:05 -0500

    Time: 103 ms (59.85%), Space: 44.8 MB (69.02%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.js
+++ 684-redundant-connection/684-redundant-connection.js
@@ -5,32 +5,32 @@
 var findRedundantConnection = function(edges) {
     const root = [0];
     const rank = [];
-    
     for (let i = 0; i < edges.length; i++) {
-        rank.push(1);
         root.push(i + 1);
+        rank.push(1);
     }
-
-    const find = (x) => root[x] === x ? root[x] : find(root[x]);
+    
+    const find = x => x === root[x] ? x : find(root[x]);
     
     const union = (x, y) => {
-        let rootX = find(x);
-        let rootY = find(y);
+        const rootX = find(x);
+        const rootY = find(y);
         
-        if (rootX === rootY) return false
+        if (rootX === rootY) return false;
         
         if (rank[rootX] > rank[rootY]) {
-            root[rootY] = rootX;
-        } else if (rank[rootX] < rank[rootY]){
+            root[rootY] = rootX
+        } else if (rank[rootX] < rank[rootY]) {
             root[rootX] = rootY;
         } else {
             root[rootY] = rootX;
             rank[rootX]++;
         }
+        
         return true;
     }
     
-    for (const [x, y] of edges) {
-        if (!union(x, y)) return [x, y];
+    for (const edge of edges) {
+        if (!union(edge[0],edge[1])) return edge;
     }
 };
