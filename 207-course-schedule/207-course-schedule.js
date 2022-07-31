006c58efbb479e84031648bd32dd60af9c537c8c
Matthew Teran <59574887+matt-teran@users.noreply.github.com>
2022-07-31 15:29:55 -0500

    Time: 68 ms (99.62%), Space: 46.9 MB (71.67%) - LeetHub

--- 207-course-schedule/207-course-schedule.js
+++ 207-course-schedule/207-course-schedule.js
@@ -6,27 +6,27 @@
 var canFinish = function(numCourses, prerequisites) {
     const adj = {};
     const visited = new Set();
-    
     for (let i = 0; i < numCourses; i++) adj[i] = [];
-    for (let [post, pre] of prerequisites) adj[post].push(pre);
+    for (const [post, pre] of prerequisites) adj[post].push(pre);
     
     const dfs = (course) => {
-        if (!adj[course].length) return true;
+        if (adj[course].length === 0) return true;
         if (visited.has(course)) return false;
         
         visited.add(course);
-        
         for (const prereq of adj[course]) {
-            if (!dfs(prereq)) return false;
+            if (!dfs(prereq)) return false;    
         }
         visited.delete(course);
         
         adj[course] = [];
+        
         return true;
     }
     
     for (let i = 0; i < numCourses; i++) {
         if (!dfs(i)) return false;
     }
+    
     return true;
 };
